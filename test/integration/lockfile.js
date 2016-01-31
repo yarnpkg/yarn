f8205cbd8f7336de5bd468515b6f57b24a46ac5e
Sebastian McKenzie <sebmck@gmail.com>
2016-01-31 22:58:39 +0000

    add tests for lockfile

---
+++ test/integration/lockfile.js
@@ -0,0 +1,20 @@
+import parse from "../../lib/lockfile/parse";
+import stringify from "../../lib/lockfile/stringify";
+
+let nullify = require("../../lib/util/map");
+let test    = require("ava");
+
+let objs = [
+  { foo: "bar" },
+  { foo: {} },
+  { foo: "foo", bar: "bar" },
+  require("../../package.json")
+];
+
+test("lockfile.parse/stringify", (t) => {
+  t.plan(objs.length);
+
+  for (let obj of objs) {
+    t.same(parse(stringify(obj)), nullify(obj));
+  }
+});
---
+++ test/unit/lockfile.js
@@ -0,0 +1,21 @@
+import parse from "../../lib/lockfile/parse";
+import stringify from "../../lib/lockfile/stringify";
+
+let nullify = require("../../lib/util/map");
+let test    = require("ava");
+
+test("lockfile.parse", (t) => {
+  t.plan(6);
+
+  t.same(parse(`foo "bar"`), nullify({ foo: "bar" }));
+  t.same(parse(`"foo" "bar"`), nullify({ foo: "bar" }));
+  t.same(parse(`foo "bar"`), nullify({ foo: "bar" }));
+
+  t.same(parse(`foo:\n  bar "bar"`), nullify({ foo: { bar: "bar" } }));
+  t.same(parse(`foo:\n  bar:\n  foo "bar"`), nullify({ foo: { bar: {}, foo: "bar" } }));
+  t.same(parse(`foo:\n  bar:\n    foo "bar"`), nullify({ foo: { bar: { foo: "bar" } } }));
+});
+
+test("lockfile.stringify", (t) => {
+
+});
