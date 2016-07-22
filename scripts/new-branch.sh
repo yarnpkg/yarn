4e1dd38c868f964dd2ccff99f340f320ed0cc8b2
Konstantin Raev <bestander@fb.com>
2016-07-22 20:05:36 +0100

    0.8.0

--- scripts/new-branch.sh
+++ scripts/new-branch.sh
@@ -1,5 +1,5 @@
 VERSION=$(npm version minor)
-BRANCH=$(echo VERSION | (IFS=".$IFS"; read a b c && echo $a.$b-stable))
+BRANCH=$(echo $VERSION | (IFS=".$IFS"; read a b c && echo $a.$b-stable))
 echo $BRANCH
 #git checkout -b $BRANCH
 #git push origin master --tags
