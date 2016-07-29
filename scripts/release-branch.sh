f3f49525fab3dd82b5dd7e24b77ca400aabbf0a7
Konstantin Raev <bestander@gmail.com>
2016-07-29 20:59:56 +0100

    rebase (#162)

---
+++ scripts/release-branch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -ex
+set -o pipefail
+
+npm version minor
+VERSION=$(node -p -e "require('./package.json').version")
+BRANCH=$(echo "$VERSION" | (IFS="."; read a b c && echo $a.$b-stable))
+echo "$BRANCH"
+git checkout -b "$BRANCH"
+git push origin master --follow-tags
+git push origin "$BRANCH" --follow-tags
