81f282ed8fc8d4fee6af6beb7bc3ce0452b58e53
Sebastian McKenzie <kittens@users.noreply.github.com>
2016-08-23 21:54:36 +0100

    add EventReporter

---
+++ src/reporters/EventReporter.js
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2016-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @flow
+ */
+
+import JSONReporter from './JSONReporter.js';
+
+const {EventEmitter} = require('events');
+
+export default class EventReporter extends JSONReporter {
+  constructor(opts: Object) {
+    super(opts);
+
+    // $FlowFixMe: looks like a flow bug
+    EventEmitter.call(this);
+  }
+
+  _dump(type: string, data: any) {
+    // $FlowFixMe: this is here! we have no `implements` in Flow though...
+    this.emit(type, data);
+  }
+}
+
+// $FlowFixMe: need to "inherit" from it
+Object.assign(EventReporter.prototype, EventEmitter.prototype);
