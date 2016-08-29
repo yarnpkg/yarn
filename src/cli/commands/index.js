a6be47cc2e71df7b6f350633ea861b92e3c21c60
Konstantin Raev <bestander@fb.com>
2016-08-29 02:25:18 +0100

    removed unnecessary change

--- src/cli/commands/index.js
+++ src/cli/commands/index.js
@@ -72,9 +72,6 @@ export {upgrade};
 import * as check from './check.js';
 export {check};
 
-import * as version from './version.js';
-export {version};
-
 import buildUseless from './_useless.js';
 
 export let runScript = buildUseless(
