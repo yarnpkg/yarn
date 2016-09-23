42849f83b605787910ce7c982edbf6eb10b2006b
Daniel Lo Nigro <daniel@dan.cx>
2016-09-22 22:27:54 -0700

    Adjustments for Debian scripts:  - Add copyright file  - Add full description to control file  - Ignore some lintian rules  - Delete clowny files that aren't needed and break lintian rules  - Update shebang in yarn.js from "node" to "nodejs" as per Debian package of Node

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -23,12 +23,22 @@ tar zxf $TARBALL_NAME -C $PACKAGE_TMPDIR/
 # Create Debian structure
 mkdir -p $PACKAGE_TMPDIR/DEBIAN
 mkdir -p $PACKAGE_TMPDIR/usr/share/yarn/
+mkdir -p $PACKAGE_TMPDIR/usr/share/doc/yarn/
+mkdir -p $PACKAGE_TMPDIR/usr/share/lintian/overrides/
 mv $PACKAGE_TMPDIR/dist/bin $PACKAGE_TMPDIR/usr/share/yarn/
 mv $PACKAGE_TMPDIR/dist/lib $PACKAGE_TMPDIR/usr/share/yarn/
 mv $PACKAGE_TMPDIR/dist/lib-legacy $PACKAGE_TMPDIR/usr/share/yarn/
 mv $PACKAGE_TMPDIR/dist/node_modules $PACKAGE_TMPDIR/usr/share/yarn/
 mv $PACKAGE_TMPDIR/dist/package.json $PACKAGE_TMPDIR/usr/share/yarn/
-mv $PACKAGE_TMPDIR/dist/LICENSE $PACKAGE_TMPDIR/DEBIAN/copyright
+cp resources/debian/copyright $PACKAGE_TMPDIR/usr/share/doc/yarn/copyright
+cp resources/debian/lintian-overrides $PACKAGE_TMPDIR/usr/share/lintian/overrides/yarn
+
+# These are unneeded and throw lintian lint errors
+rm -f $PACKAGE_TMPDIR/usr/share/yarn/node_modules/node-uuid/benchmark/bench.gnu
+find $PACKAGE_TMPDIR/usr/share/yarn \( -name '*.md' -o  -name '*.md~' -o -name '*.gitmodules' \) -delete
+
+# Assume everything else is junk we don't need
+rm -rf $PACKAGE_TMPDIR/dist
 
 # Currently the "binaries" are JavaScript files that expect to be in the same
 # directory as the libraries, so we can't just copy them directly to /usr/bin.
@@ -36,8 +46,8 @@ mv $PACKAGE_TMPDIR/dist/LICENSE $PACKAGE_TMPDIR/DEBIAN/copyright
 mkdir -p $PACKAGE_TMPDIR/usr/bin/
 ln -s ../share/yarn/bin/yarn.js $PACKAGE_TMPDIR/usr/bin/yarn
 
-# Assume everything else is junk we don't need
-rm -rf $PACKAGE_TMPDIR/dist
+# Debian/Ubuntu call the Node.js binary "nodejs", not "node".
+sed -i 's/env node/env nodejs/' $PACKAGE_TMPDIR/usr/share/yarn/bin/yarn.js
 
 # Replace variables in Debian package control file
 INSTALLED_SIZE=`du -sk $PACKAGE_TMPDIR | cut -f 1`
@@ -47,3 +57,5 @@ sed -e "s/\$VERSION/$VERSION/;s/\$INSTALLED_SIZE/$INSTALLED_SIZE/" < resources/d
 fakeroot dpkg-deb -b $PACKAGE_TMPDIR $PACKAGE_NAME
 mv $PACKAGE_NAME dist/
 rm -rf $PACKAGE_TMPDIR
+
+lintian dist/$PACKAGE_NAME
