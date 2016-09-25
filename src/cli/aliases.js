66cccb6dba91fa5c21fd32f263aec3e1cef10bb2
Sebastian McKenzie <kittens@users.noreply.github.com>
2016-09-25 05:12:40 +0100

    turn aliases back into js

---
+++ src/cli/aliases.js
@@ -0,0 +1,15 @@
+/* @flow */
+
+export default {
+  i: 'install',
+  t: 'test',
+  update: 'upgrade',
+  ddp: 'dedupe',
+  'find-dupes': 'dedupe',
+  rm: 'remove',
+  r: 'remove',
+  uninstall: 'remove',
+  unlink: 'remove',
+  runScript: 'run',
+  'run-script': 'run',
+};
