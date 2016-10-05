e5da478e055db80c7a6c99b33653f87b564ee932
Sebastian McKenzie <kittens@users.noreply.github.com>
2016-10-05 16:44:59 +0100

    add `generate-lock-entry` command - fixes #198, fixes #342 (#516)

---
+++ src/cli/commands/generate-lock-entry.js
@@ -0,0 +1,58 @@
+/* @flow */
+
+import type {Reporter} from '../../reporters/index.js';
+import type Config from '../../config.js';
+import {MessageError} from '../../errors.js';
+import {implodeEntry} from '../../lockfile/wrapper.js';
+import stringify from '../../lockfile/stringify.js';
+import * as fs from '../../util/fs.js';
+
+export function hasWrapper(): boolean {
+  return false;
+}
+
+export async function run(
+  config: Config,
+  reporter: Reporter,
+  flags: Object,
+  args: Array<string>,
+): Promise<void> {
+  let manifest;
+  if (flags.useManifest) {
+    manifest = await fs.readJson(flags.useManifest);
+  } else {
+    manifest = await config.readRootManifest();
+  }
+  if (!manifest.name) {
+    throw new MessageError(reporter.lang('noName'));
+  }
+  if (!manifest.version) {
+    throw new MessageError(reporter.lang('noVersion'));
+  }
+
+  const entry = {
+    name: manifest.name,
+    version: manifest.version,
+    resolved: flags.resolved,
+    registry: flags.registry || manifest._registry,
+    optionalDependencies: manifest.optionalDependencies,
+    dependencies: manifest.dependencies,
+  };
+  const pattern = flags.pattern || `${entry.name}@${entry.version}`;
+  reporter.log(stringify({
+    [pattern]: implodeEntry(pattern, entry),
+  }));
+}
+
+export function setFlags(commander: Object) {
+  commander.option('--use-manifest <location>', 'description');
+  commander.option('--resolved <resolved>', 'description');
+  commander.option('--registry <registry>', 'description');
+}
+
+export const examples = [
+  'generate-lock-entry',
+  'generate-lock-entry --use-manifest ./package.json',
+  'generate-lock-entry --registry bower',
+  'generate-lock-entry --resolved local-file.tgz#hash',
+];
