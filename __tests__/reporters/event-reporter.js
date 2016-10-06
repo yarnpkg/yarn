e638b7e621d970cb49ade111bdba9bbe40565e7e
Sebastian McKenzie <kittens@users.noreply.github.com>
2016-10-06 14:56:11 +0100

    add more reporter tests

---
+++ __tests__/reporters/event-reporter.js
@@ -0,0 +1,66 @@
+/* @flow */
+
+import EventReporter from '../../src/reporters/event-reporter.js';
+import build from './_mock.js';
+
+type Events = Array<Object>;
+
+const getBuff = build(
+  EventReporter,
+  (data, reporter: EventReporter, events: Events): Events => events,
+  (reporter: EventReporter): Events => {
+    const events: Events = [];
+    reporter.emit = (type: string, data: any) => {
+      events.push({type, data});
+    };
+    return events;
+  },
+);
+
+test('EventReporter.finished', async () => {
+  expect(await getBuff((r) => {
+    r.footer(false);
+  })).toMatchSnapshot();
+});
+
+test('EventReporter.step', async () => {
+  expect(await getBuff((r) => {
+    r.step(1, 5, 'foobar');
+  })).toMatchSnapshot();
+});
+
+test('EventReporter.log', async () => {
+  expect(await getBuff((r) => {
+    r.log('foobar');
+  })).toMatchSnapshot();
+});
+
+test('EventReporter.success', async () => {
+  expect(await getBuff((r) => {
+    r.success('foobar');
+  })).toMatchSnapshot();
+});
+
+test('EventReporter.error', async () => {
+  expect(await getBuff((r) => {
+    r.error('foobar');
+  })).toMatchSnapshot();
+});
+
+test('EventReporter.info', async () => {
+  expect(await getBuff((r) => {
+    r.info('foobar');
+  })).toMatchSnapshot();
+});
+
+test('EventReporter.command', async () => {
+  expect(await getBuff((r) => {
+    r.command('foobar');
+  })).toMatchSnapshot();
+});
+
+test('EventReporter.warn', async () => {
+  expect(await getBuff((r) => {
+    r.warn('foobar');
+  })).toMatchSnapshot();
+});
