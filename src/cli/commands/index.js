6226c6eae442a240ab148eed6803a5a2cd823c2f
cpojer <christoph.pojer@gmail.com>
2016-10-07 15:36:44 +0900

    Add `bin` command because it seems like something people might use.

--- src/cli/commands/index.js
+++ src/cli/commands/index.js
@@ -1,91 +1,35 @@
 /* @flow */
 
-import * as cache from './cache.js';
-export {cache};
-
-import * as ls from './ls.js';
-export {ls};
-
-import * as why from './why.js';
-export {why};
-
-import * as access from './access.js';
-export {access};
-
-import * as global from './global.js';
-export {global};
-
-import * as add from './add.js';
-export {add};
-
-import * as clean from './clean.js';
-export {clean};
-
-import * as config from './config.js';
-export {config};
-
-import * as team from './team.js';
-export {team};
-
-import * as generateLockEntry from './generate-lock-entry.js';
-export {generateLockEntry};
-
-import * as link from './link.js';
-export {link};
-
-import * as unlink from './unlink.js';
-export {unlink};
-
-import * as info from './info.js';
-export {info};
-
-import * as init from './init.js';
-export {init};
-
-import * as outdated from './outdated.js';
-export {outdated};
-
-import * as pack from './pack.js';
-export {pack};
-
-import * as owner from './owner.js';
-export {owner};
-
-import * as tag from './tag.js';
-export {tag};
-
-import * as publish from './publish.js';
-export {publish};
-
-import * as login from './login.js';
-export {login};
-
-import * as logout from './logout.js';
-export {logout};
-
-import * as licenses from './licenses.js';
-export {licenses};
-
-import * as selfUpdate from './self-update.js';
-export {selfUpdate};
-
-import * as remove from './remove.js';
-export {remove};
-
-import * as install from './install.js';
-export {install};
-
-import * as upgrade from './upgrade.js';
-export {upgrade};
-
-import * as check from './check.js';
-export {check};
-
-import * as version from './version.js';
-export {version};
-
-import * as run from './run.js';
-export {run};
+import * as access from './access.js'; export {access};
+import * as add from './add.js'; export {add};
+import * as bin from './bin.js'; export {bin};
+import * as cache from './cache.js'; export {cache};
+import * as check from './check.js'; export {check};
+import * as clean from './clean.js'; export {clean};
+import * as config from './config.js'; export {config};
+import * as generateLockEntry from './generate-lock-entry.js'; export {generateLockEntry};
+import * as global from './global.js'; export {global};
+import * as info from './info.js'; export {info};
+import * as init from './init.js'; export {init};
+import * as install from './install.js'; export {install};
+import * as licenses from './licenses.js'; export {licenses};
+import * as link from './link.js'; export {link};
+import * as login from './login.js'; export {login};
+import * as logout from './logout.js'; export {logout};
+import * as ls from './ls.js'; export {ls};
+import * as outdated from './outdated.js'; export {outdated};
+import * as owner from './owner.js'; export {owner};
+import * as pack from './pack.js'; export {pack};
+import * as publish from './publish.js'; export {publish};
+import * as remove from './remove.js'; export {remove};
+import * as run from './run.js'; export {run};
+import * as selfUpdate from './self-update.js'; export {selfUpdate};
+import * as tag from './tag.js'; export {tag};
+import * as team from './team.js'; export {team};
+import * as unlink from './unlink.js'; export {unlink};
+import * as upgrade from './upgrade.js'; export {upgrade};
+import * as version from './version.js'; export {version};
+import * as why from './why.js'; export {why};
 
 import buildUseless from './_useless.js';
 
