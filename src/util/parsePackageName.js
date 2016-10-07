778572b8e374c4112660ebf52af8b75ae6236eb1
cpojer <christoph.pojer@gmail.com>
2016-10-07 14:04:15 +0900

    Add support for `yarn outdated [list of packages]`

---
+++ src/util/parsePackageName.js
@@ -0,0 +1,13 @@
+/* @flow */
+
+type PackageInput = {
+  name: string,
+  version: ?string,
+};
+
+const PKG_INPUT = /(^\S[^\s@]+)(?:@(\S+))?$/;
+
+export default function parsePackageName(input: string): PackageInput {
+  const [, name, version] = PKG_INPUT.exec(input);
+  return {name, version};
+}
