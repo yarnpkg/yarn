d35ee2a742d018438110441f3a3309e5d8837f7c
Roger Nesbitt <roger@seriousorange.com>
2016-10-12 11:59:05 +1300

    Support unimplemented call in WSL (#753)
    Yarn works fine on Windows Subsystem for Linux (WSL) apart from a single call
    to enumerate network interfaces.  If this call fails on WSL, we instead assume
    that the user has an internet connection and continue.

--- src/util/network.js
+++ src/util/network.js
@@ -6,7 +6,20 @@ const IGNORE_INTERFACES = ['lo0', 'awdl0', 'bridge0'];
 const LOCAL_IPS = ['127.0.0.1', '::1'];
 
 export function isOffline(): boolean {
-  const interfaces = os.networkInterfaces();
+  let interfaces;
+
+  try {
+    interfaces = os.networkInterfaces();
+  } catch (e) {
+    // As of October 2016, Windows Subsystem for Linux (WSL) does not support
+    // the os.networkInterfaces() call and throws instead. For this platform,
+    // assume we are online.
+    if (e.syscall === 'uv_interface_addresses') {
+      return false;
+    } else {
+      throw e;
+    }
+  }
 
   for (const name in interfaces) {
     if (IGNORE_INTERFACES.indexOf(name) >= 0) {
