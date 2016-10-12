c9f8dfee8acdad3803c7300bfe3175df3370da9f
Neil Kistner <neil.kistner@gmail.com>
2016-10-11 19:54:32 -0500

    Remove commander's `allowUnknownOption` method call

--- src/cli/index.js
+++ src/cli/index.js
@@ -57,7 +57,6 @@ commander.option(
   '--mutex [type][:specifier]',
   'use a mutex to ensure only one yarn instance is executing',
 );
-commander.allowUnknownOption();
 
 // get command name
 let commandName: string = args.shift() || '';
