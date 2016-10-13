c99ff4dff3950dad5aa9d2d744e2872240bec7fc
Kasper Lewau <lewauu@gmail.com>
2016-10-13 10:52:15 +0200

    fix(packageLinker): ensure dir before symlinking bin (#975)

--- src/package-linker.js
+++ src/package-linker.js
@@ -26,6 +26,7 @@ export async function linkBin(src: string, dest: string): Promise<void> {
   if (process.platform === 'win32') {
     await cmdShim(src, dest);
   } else {
+    await fs.mkdirp(path.dirname(dest));
     await fs.symlink(src, dest);
     await fs.chmod(dest, '755');
   }
