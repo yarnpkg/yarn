yarnpkg/yarn

commit 369539f7553c74deb04c080cc140187eeb2c5f5e

Fix custom registries with paths (#712)
Ensures with and without trailing / registry url is correct and resolvable.

src/registries/npm-registry.js
@@ -54,8 +54,11 @@ export default class NpmRegistry extends Registry {
       headers.authorization = `Bearer ${this.token}`;
     }
 
+    // $FlowFixMe : https://github.com/facebook/flow/issues/908
+    const requestUrl = url.format(`${registry}/${pathname}`);
+
     return this.requestManager.request({
-      url: url.resolve(registry, pathname),
+      url: requestUrl,
       method: opts.method,
       body: opts.body,
       auth: opts.auth,

commit a3eaa55e4b39813c83f3cf86f3611438b59e922f

Support npmrc for private registries and auth (#839)
* Get npm registry and auth from .npmrc file

* Fetch tarball through registry

Needed if the registry requires auth to get tarballs

* Support .npmrc auth for write commands

* Support basic auth

* Attempt to fix flow

* Implement default request method for BaseRegistry

Needed for tarball fetching now that it is going through the registry

* Fix flow bug

* Clean up

* Fix requestUrl

* Broken by #712, which changed from url.resolve to url.format. We need resolve in the case where the pathname is really a full url (when requesting tarballs). So only add the trailing / if it is not there.
* Only add authorization if the request is going to the registry (not random tarballs).

* Check scoped always-auth option

* Remove unused function (bad rebase)

src/cli/commands/login.js
@@ -33,12 +33,21 @@ async function getCredentials(config: Config, reporter: Reporter): Promise<?{
   return {username, email};
 }
 
-export async function getToken(config: Config, reporter: Reporter): Promise<
+export async function getToken(config: Config, reporter: Reporter, name: string = ''): Promise<
   () => Promise<void>
 > {
+  const auth = config.registries.npm.getAuth(name);
+  if (auth) {
+    config.registries.npm.setToken(auth);
+    return function revoke(): Promise<void> {
+      reporter.info(reporter.lang('notRevokingConfigToken'));
+      return Promise.resolve();
+    };
+  }
+
   const env = process.env.YARN_AUTH_TOKEN || process.env.KPM_AUTH_TOKEN || process.env.NPM_AUTH_TOKEN;
   if (env) {
-    config.registries.npm.setToken(env);
+    config.registries.npm.setToken(`Bearer ${env}`);
     return function revoke(): Promise<void> {
       reporter.info(reporter.lang('notRevokingEnvToken'));
       return Promise.resolve();
@@ -80,7 +89,7 @@ export async function getToken(config: Config, reporter: Reporter): Promise<
     reporter.success(reporter.lang('loggedIn'));
 
     const token = res.token;
-    config.registries.npm.setToken(token);
+    config.registries.npm.setToken(`Bearer ${token}`);
 
     return async function revoke(): Promise<void> {
       reporter.success(reporter.lang('revokedToken'));

src/cli/commands/owner.js
@@ -34,7 +34,7 @@ export async function mutate(
 
   const msgs = buildMessages(username, name);
   reporter.step(1, 3, reporter.lang('loggingIn'));
-  const revoke = await getToken(config, reporter);
+  const revoke = await getToken(config, reporter, name);
 
   reporter.step(2, 3, msgs.info);
   const user = await config.registries.npm.request(`-/user/org.couchdb.user:${username}`);
@@ -160,7 +160,7 @@ export const {run, setFlags} = buildSubCommands('owner', {
     const name = await getName(args, config);
 
     reporter.step(1, 3, reporter.lang('loggingIn'));
-    const revoke = await getToken(config, reporter);
+    const revoke = await getToken(config, reporter, name);
 
     reporter.step(2, 3, reporter.lang('ownerGetting', name));
     const pkg = await config.registries.npm.request(name);

src/cli/commands/publish.js
@@ -138,7 +138,7 @@ export async function run(
 
   //
   reporter.step(2, 4, reporter.lang('loggingIn'));
-  const revoke = await getToken(config, reporter);
+  const revoke = await getToken(config, reporter, pkg.name);
 
   //
   reporter.step(3, 4, reporter.lang('publishing'));

src/cli/commands/tag.js
@@ -50,7 +50,7 @@ export const {run, setFlags, examples} = buildSubCommands('tag', {
     const tag = args.shift();
 
     reporter.step(1, 3, reporter.lang('loggingIn'));
-    const revoke = await getToken(config, reporter);
+    const revoke = await getToken(config, reporter, name);
 
     reporter.step(2, 3, reporter.lang('creatingTag', tag, range));
     const result = await config.registries.npm.request(
@@ -91,7 +91,7 @@ export const {run, setFlags, examples} = buildSubCommands('tag', {
     const tag = args.shift();
 
     reporter.step(1, 3, reporter.lang('loggingIn'));
-    const revoke = await getToken(config, reporter);
+    const revoke = await getToken(config, reporter, name);
 
     reporter.step(2, 3, reporter.lang('deletingTags'));
     const result = await config.registries.npm.request(`-/package/${name}/dist-tags/${encodeURI(tag)}`, {
@@ -120,11 +120,12 @@ export const {run, setFlags, examples} = buildSubCommands('tag', {
     flags: Object,
     args: Array<string>,
   ): Promise<void> {
+    const name = await getName(args, config);
+
     reporter.step(1, 3, reporter.lang('loggingIn'));
-    const revoke = await getToken(config, reporter);
+    const revoke = await getToken(config, reporter, name);
 
     reporter.step(2, 3, reporter.lang('gettingTags'));
-    const name = await getName(args, config);
     const tags = await config.registries.npm.request(`-/package/${name}/dist-tags`);
 
     if (tags) {

src/config.js
@@ -1,8 +1,7 @@
 /* @flow */
 
-import type {RegistryNames} from './registries/index.js';
+import type {RegistryNames, ConfigRegistries} from './registries/index.js';
 import type {Reporter} from './reporters/index.js';
-import type Registry from './registries/base-registry.js';
 import type {Manifest, PackageRemote} from './types.js';
 import normalizeManifest from './util/normalize-manifest/index.js';
 import {MessageError} from './errors.js';
@@ -42,7 +41,6 @@ type PackageMetadata = {
   package: Manifest
 };
 
-
 type RootManifests = {
   [registryName: RegistryNames]: {
     loc: string,
@@ -59,10 +57,6 @@ function sortObject(object: Object): Object {
   });
   return sortedObject;
 }
-
-export type ConfigRegistries = {
-  [name: RegistryNames]: Registry
-};
 
 export default class Config {
   constructor(reporter: Reporter) {

src/fetchers/tarball-fetcher.js
@@ -130,9 +130,9 @@ export default class TarballFetcher extends BaseFetcher {
 
   fetchFromExternal(): Promise<FetchedOverride> {
     const {reference: ref} = this;
+    const registry = this.config.registries[this.registry];
 
-    return this.config.requestManager.request({
-      url: ref,
+    return registry.request(ref, {
       headers: {
         'Accept-Encoding': 'gzip',
         'Accept': 'application/octet-stream',

src/registries/base-registry.js
@@ -1,7 +1,8 @@
 /* @flow */
 
 import type RequestManager, {RequestMethods} from '../util/request-manager.js';
-import type Config, {ConfigRegistries} from '../config.js';
+import type Config from '../config.js';
+import type {ConfigRegistries} from './index.js';
 import {removePrefix} from '../util/misc.js';
 
 const objectPath = require('object-path');
@@ -11,6 +12,8 @@ export type RegistryRequestOptions = {
   method?: RequestMethods,
   auth?: Object,
   body?: mixed,
+  buffer?: bool,
+  process?: Function
 };
 
 export type CheckOutdatedReturn = Promise<{
@@ -73,8 +76,11 @@ export default class BaseRegistry {
     return Promise.reject(new Error('unimplemented'));
   }
 
-  request(pathname: string, opts?: RegistryRequestOptions = {}): Promise<?Object> {
-    return Promise.reject(new Error('unimplemented'));
+  request(pathname: string, opts?: RegistryRequestOptions = {}): Promise<*> {
+    return this.requestManager.request({
+      url: pathname,
+      ...opts,
+    });
   }
 
   async init(): Promise<void> {

src/registries/index.js
@@ -13,3 +13,8 @@ export const registries = {
 export const registryNames = Object.keys(registries);
 
 export type RegistryNames = $Keys<typeof registries>;
+export type ConfigRegistries = {
+  npm: NpmRegistry,
+  yarn: YarnRegistry,
+  bower: BowerRegistry
+};

src/registries/npm-registry.js
@@ -2,17 +2,20 @@
 
 import type RequestManager from '../util/request-manager.js';
 import type {RegistryRequestOptions, CheckOutdatedReturn} from './base-registry.js';
-import type Config, {ConfigRegistries} from '../config.js';
+import type Config from '../config.js';
+import type {ConfigRegistries} from './index.js';
 import * as fs from '../util/fs.js';
 import NpmResolver from '../resolvers/registries/npm-resolver.js';
 import Registry from './base-registry.js';
-import {removeSuffix} from '../util/misc.js';
+import {addSuffix} from '../util/misc';
 
 const defaults = require('defaults');
 const userHome = require('user-home');
 const path = require('path');
 const url = require('url');
 const ini = require('ini');
+
+const DEFAULT_REGISTRY = 'https://registry.npmjs.org/';
 
 function getGlobalPrefix(): string {
   if (process.env.PREFIX) {
@@ -46,16 +49,16 @@ export default class NpmRegistry extends Registry {
     return name.replace('/', '%2f');
   }
 
-  request(pathname: string, opts?: RegistryRequestOptions = {}): Promise<?Object> {
-    const registry = removeSuffix(String(this.registries.yarn.getOption('registry')), '/');
+  request(pathname: string, opts?: RegistryRequestOptions = {}): Promise<*> {
+    const registry = addSuffix(this.getRegistry(pathname), '/');
+    const requestUrl = url.resolve(registry, pathname);
+    const alwaysAuth = this.getScopedOption(registry.replace(/^https?:/, ''), 'always-auth')
+                    || this.getOption('always-auth');
 
     const headers = {};
-    if (this.token) {
-      headers.authorization = `Bearer ${this.token}`;
+    if (this.token || (alwaysAuth && requestUrl.startsWith(registry))) {
+      headers.authorization = this.getAuth(pathname);
     }
-
-    // $FlowFixMe : https://github.com/facebook/flow/issues/908
-    const requestUrl = url.format(`${registry}/${pathname}`);
 
     return this.requestManager.request({
       url: requestUrl,
@@ -63,7 +66,9 @@ export default class NpmRegistry extends Registry {
       body: opts.body,
       auth: opts.auth,
       headers,
-      json: true,
+      json: !opts.buffer,
+      buffer: opts.buffer,
+      process: opts.process,
       gzip: true,
     });
   }
@@ -128,5 +133,58 @@ export default class NpmRegistry extends Registry {
 
       defaults(this.config, config);
     }
+  }
+
+  getScope(packageName: string): string {
+    return !packageName || packageName[0] !== '@' ? '' : packageName.split(/\/|%2f/)[0];
+  }
+
+  getRegistry(packageName: string): string {
+    // Try scoped registry, and default registry
+    for (const scope of [this.getScope(packageName), '']) {
+      const registry = this.getScopedOption(scope, 'registry')
+                    || this.registries.yarn.getScopedOption(scope, 'registry');
+      if (registry) {
+        return String(registry);
+      }
+    }
+
+    return DEFAULT_REGISTRY;
+  }
+
+  getAuth(packageName: string): string {
+    if (this.token) {
+      return this.token;
+    }
+
+    for (let registry of [this.getRegistry(packageName), '', DEFAULT_REGISTRY]) {
+      registry = registry.replace(/^https?:/, '');
+
+      // Check for bearer token.
+      let auth = this.getScopedOption(registry, '_authToken');
+      if (auth) {
+        return `Bearer ${String(auth)}`;
+      }
+
+      // Check for basic auth token.
+      auth = this.getScopedOption(registry, '_auth');
+      if (auth) {
+        return `Basic ${String(auth)}`;
+      }
+
+      // Check for basic username/password auth.
+      const username = this.getScopedOption(registry, 'username');
+      const password = this.getScopedOption(registry, '_password');
+      if (username && password) {
+        const pw = new Buffer(String(password), 'base64').toString();
+        return 'Basic ' + new Buffer(String(username) + ':' + pw).toString('base64');
+      }
+    }
+
+    return '';
+  }
+
+  getScopedOption(scope: string, option: string): mixed {
+    return this.getOption(scope + (scope ? ':' : '') + option);
   }
 }

src/registries/yarn-registry.js
@@ -1,7 +1,7 @@
 /* @flow */
 
 import type RequestManager from '../util/request-manager.js';
-import type {ConfigRegistries} from '../config.js';
+import type {ConfigRegistries} from './index.js';
 import {YARN_REGISTRY} from '../constants.js';
 import NpmRegistry from './npm-registry.js';
 import stringify from '../lockfile/stringify.js';

src/reporters/lang/en.js
@@ -182,6 +182,7 @@ const messages = {
   loggingIn: 'Logging in',
   loggedIn: 'Logged in.',
   notRevokingEnvToken: 'Not revoking login token, specified via environment variable.',
+  notRevokingConfigToken: 'Not revoking login token, specified via config file.',
   noTokenToRevoke: 'No login token to revoke.',
   revokingToken: 'Revoking token',
   revokedToken: 'Revoked login token.',

src/util/misc.js
@@ -30,3 +30,11 @@ export function removeSuffix(pattern: string, suffix: string): string {
 
   return pattern;
 }
+
+export function addSuffix(pattern: string, suffix: string): string {
+  if (!pattern.endsWith(suffix)) {
+    return pattern + suffix;
+  }
+
+  return pattern;
+}