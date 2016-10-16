8f4f01e89c5b0d3e677f327bff5017adb81fcdf4
Daniel Lo Nigro <daniel@dan.cx>
2016-10-15 21:45:38 -0700

    Use Yarn package/installer on Travis/AppVeyor/CircleCI rather than npm (#1117)

--- scripts/build-dist.sh
+++ scripts/build-dist.sh
@@ -12,6 +12,7 @@ cd dist
 umask 0022 # Ensure permissions are correct (0755 for dirs, 0644 for files)
 tar -xzf pack.tgz --strip 1
 rm -rf pack.tgz
+# Change this to "yarn install --production" once #1115 is fixed
 npm install --production
 rm -rf node_modules/*/test node_modules/*/dist
 cd ..
