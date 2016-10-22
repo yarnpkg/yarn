20ce761678aa205241383144ebd5ae7aedc0cc43
Steve Shreeve <steve.shreeve@gmail.com>
2016-10-22 13:47:51 -0400

    honor github commit hash

--- src/resolvers/exotics/github-resolver.js
+++ src/resolvers/exotics/github-resolver.js
@@ -26,7 +26,7 @@ export default class GitHubResolver extends HostedGitResolver {
   }
 
   static getGitSSHUrl(parts: ExplodedFragment): string {
-    return `git@github.com:${parts.user}/${parts.repo}.git`;
+    return `git@github.com:${parts.user}/${parts.repo}.git${parts.hash ? '#' + decodeURIComponent(parts.hash) : ''}`;
   }
 
   static getGitHTTPUrl(parts: ExplodedFragment): string {
