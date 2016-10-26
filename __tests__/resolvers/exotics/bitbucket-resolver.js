3cd994977a7d804d5483d6671624f60a5aa398c6
Nick Olinger <olingern@gmail.com>
2016-10-26 09:16:36 -0400

    Tests/resolvers/exotics (#1470)
    * Move github-resolver to match src dir structure
    
    * Add bitbucket resolver tests

---
+++ __tests__/resolvers/exotics/bitbucket-resolver.js
@@ -0,0 +1,61 @@
+/* @flow */
+
+import BitBucketResolver from '../../../src/resolvers/exotics/bitbucket-resolver.js';
+import type {ExplodedFragment} from '../../../src/resolvers/exotics/hosted-git-resolver.js';
+
+const _bitBucketBase = 'https://bitbucket.org/';
+
+test('hostname should be "bitbucket.org" ', () => {
+  expect(BitBucketResolver.hostname).toBe('bitbucket.org');
+});
+
+test('protocol should be "bitbucket" ', () => {
+  expect(BitBucketResolver.protocol).toBe('bitbucket');
+});
+
+test('getTarballUrl should return the correct bitbucket tarball url', () => {
+  const fragment: ExplodedFragment = {
+    user: 'foo',
+    repo: 'bar',
+    hash: '',
+  };
+  const hash = 'baz';
+  const expected =  _bitBucketBase + fragment.user + '/' + fragment.repo + '/get/' + hash + '.tar.gz';
+  expect(BitBucketResolver.getTarballUrl(fragment, hash)).toBe(expected);
+});
+
+test('getGitHTTPUrl should return the correct git bitbucket url', () => {
+  const fragment: ExplodedFragment = {
+    user: 'foo',
+    repo: 'bar',
+    hash: '',
+  };
+
+  const expected =  _bitBucketBase + fragment.user + '/' + fragment.repo + '.git';
+  expect(BitBucketResolver.getGitHTTPUrl(fragment)).toBe(expected);
+});
+
+test('getGitHTTPUrl should return the correct git bitbucket SSH url', () => {
+  const fragment: ExplodedFragment = {
+    user: 'foo',
+    repo: 'bar',
+    hash: '',
+  };
+
+  const expected =  'git@bitbucket.org:' + fragment.user + '/' + fragment.repo + '.git';
+  expect(BitBucketResolver.getGitSSHUrl(fragment)).toBe(expected);
+});
+
+test('getHTTPFileUrl should return the correct HTTP file url', () => {
+  const fragment: ExplodedFragment = {
+    user: 'foo',
+    repo: 'bar',
+    hash: '',
+  };
+
+  const commit = 'abc123';
+  const filename = 'baz.js';
+
+  const expected =  _bitBucketBase + fragment.user + '/' + fragment.repo + '/raw/' + commit + '/' + filename;
+  expect(BitBucketResolver.getHTTPFileUrl(fragment, filename, commit)).toBe(expected);
+});
