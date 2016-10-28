ecf0aedf7c064b890b02936a53e189a84d0110bc
jvhoven <jeffreyvanhoven@gmail.com>
2016-10-28 10:39:02 +0200

    Use err.message instead of err.stack

--- src/cli/index.js
+++ src/cli/index.js
@@ -315,7 +315,7 @@ function onUnexpectedError(err: Error) {
   const errorLoc = path.join(config.cwd, 'yarn-error.log');
   fs.writeFileSync(errorLoc, log.join('\n\n') + '\n');
 
-  reporter.error(reporter.lang('unexpectedError', err.stack));
+  reporter.error(reporter.lang('unexpectedError', err.message));
   reporter.info(reporter.lang('bugReport', errorLoc));
 }
 
