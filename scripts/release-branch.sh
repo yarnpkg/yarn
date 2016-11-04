ea1502a98f43658c3ff0fc7793fbe120af01b3e1
Konstantin Raev <bestander@fb.com>
2016-11-04 17:34:25 +0000

    switched npm with yarn

--- scripts/release-branch.sh
+++ scripts/release-branch.sh
@@ -2,12 +2,12 @@
 
 set -ex
 
-npm version minor
+yarn version --new-version minor
 VERSION=$(node -p -e "require('./package.json').version")
 BRANCH=$(echo "$VERSION" | (IFS="."; read a b c && echo $a.$b-stable))
 echo "$BRANCH"
 git checkout -b "$BRANCH"
 git push origin "$BRANCH" --follow-tags
 git checkout master
-npm version preminor
+yarn version --new-version preminor
 git push origin master --follow-tags
