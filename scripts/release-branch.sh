a748ea50299ef04af40f607ac81d04c664d688c7
Konstantin Raev <bestander@fb.com>
2016-11-04 16:43:11 +0000

    bump

--- scripts/release-branch.sh
+++ scripts/release-branch.sh
@@ -7,5 +7,7 @@ VERSION=$(node -p -e "require('./package.json').version")
 BRANCH=$(echo "$VERSION" | (IFS="."; read a b c && echo $a.$b-stable))
 echo "$BRANCH"
 git checkout -b "$BRANCH"
-git push origin master --follow-tags
 git push origin "$BRANCH" --follow-tags
+git checkout master
+npm version preminor
+git push origin master --follow-tags
