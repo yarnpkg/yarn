15d5c94b5c99c413a3fdf00c386ee010fd68081e
Konstantin Raev <bestander@fb.com>
2016-11-04 16:59:54 +0000

    Revert "bump"
    This reverts commit a748ea50299ef04af40f607ac81d04c664d688c7.

--- scripts/release-branch.sh
+++ scripts/release-branch.sh
@@ -7,7 +7,5 @@ VERSION=$(node -p -e "require('./package.json').version")
 BRANCH=$(echo "$VERSION" | (IFS="."; read a b c && echo $a.$b-stable))
 echo "$BRANCH"
 git checkout -b "$BRANCH"
-git push origin "$BRANCH" --follow-tags
-git checkout master
-npm version preminor
 git push origin master --follow-tags
+git push origin "$BRANCH" --follow-tags
