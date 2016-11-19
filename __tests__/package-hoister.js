d047b1851315219721cb4c888c58b09fbf9fab87
Daniel Poindexter <dp.texas@gmail.com>
2016-11-19 06:17:24 -0600

    Resolve scoped package paths during linking on Windows (#1866)
    * Resolve scoped package paths during linking on Windows
    
    * Normalize file copy destination when creating hoist manifests
    
    * Add flow types to package-hoister unit tests

---
+++ __tests__/package-hoister.js
@@ -0,0 +1,44 @@
+/* @flow */
+
+import PackageHoister, {HoistManifest} from '../src/package-hoister.js';
+import type PackageResolver from '../src/package-resolver.js';
+import type PackageReference from '../src/package-reference.js';
+import type Config from '../src/config.js';
+import type {Manifest} from '../src/types.js';
+
+const path = require('path');
+
+test('Produces valid destination paths for scoped modules', () => {
+  const expected = path.join(__dirname, './node_modules/@scoped/dep');
+  const scopedPackageName = '@scoped/dep';
+
+  const config = (({
+    cwd: __dirname,
+    getFolder(): string {
+      return 'node_modules';
+    },
+  }: any): Config);
+
+  const resolver = (({}: any): PackageResolver);
+
+  const key = scopedPackageName;
+  const parts = [scopedPackageName];
+
+  const pkg = (({
+    _reference: (({}: any): PackageReference),
+  }: any): Manifest);
+
+  const info = new HoistManifest(key, parts, pkg, '');
+
+  const tree = new Map([
+    ['@scoped/dep', info],
+  ]);
+
+  const packageHoister = new PackageHoister(config, resolver, false);
+  packageHoister.tree = tree;
+
+  const result = packageHoister.init();
+  const [actual] = result[0];
+
+  expect(actual).toEqual(expected);
+});
