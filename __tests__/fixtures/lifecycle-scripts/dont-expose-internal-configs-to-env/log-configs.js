89801f9071ebed4521b3e218d05578df06c78930
jddxf <740531372@qq.com>
2017-01-10 17:38:35 +0800

    Use es5 in files that won't be transpiled.

--- __tests__/fixtures/lifecycle-scripts/dont-expose-internal-configs-to-env/log-configs.js
+++ __tests__/fixtures/lifecycle-scripts/dont-expose-internal-configs-to-env/log-configs.js
@@ -1,7 +1,8 @@
-const configs = {};
-for (const key in process.env) {
-  if (key.startsWith('npm_config_')) {
+// As this file won't be transpiled, we use ES5 here.
+var configs = {};
+for (var key in process.env) {
+  if (key.indexOf('npm_config_') === 0) {
     configs[key] = process.env[key];
   }
 }
-console.log(`##${JSON.stringify(configs)}##`);
+console.log('##' + JSON.stringify(configs) + '##');
