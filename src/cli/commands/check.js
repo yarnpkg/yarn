93fb3ea8a0cd8b1e1f0516e73d1a19035eeaecb8
Konstantin Raev <bestander@fb.com>
2017-01-13 12:57:49 +0000

    lint fixes

--- src/cli/commands/check.js
+++ src/cli/commands/check.js
@@ -6,7 +6,6 @@ import {MessageError} from '../../errors.js';
 import {Install} from './install.js';
 import Lockfile from '../../lockfile/wrapper.js';
 import * as fs from '../../util/fs.js';
-import Registry from '../../registries/base-registry.js';
 
 const semver = require('semver');
 const path = require('path');
@@ -25,7 +24,6 @@ async function commonJsCheck(
   flags: Object,
   args: Array<string>,
 ): Promise<void> {
-  let warningCount = 0;
   let errCount = 0;
   function reportError(msg, ...vars) {
     reporter.error(reporter.lang(msg, ...vars));
@@ -34,7 +32,7 @@ async function commonJsCheck(
   // check all dependencies recursively without relying on internal resolver
   const registryName = 'yarn';
   const registry = config.registries[registryName];
-  const pkg = await config.readManifest(registry.cwd, registryName);
+  const rootManifest = await config.readManifest(registry.cwd, registryName);
 
   type PackageToVerify = {
     name: string,
@@ -42,20 +40,21 @@ async function commonJsCheck(
     parentCwd: string,
     version: string,
   };
-  // TODO check devDependencies, optionalDependencies, peerDependencies
-  let dependenciesToCheckVersion: PackageToVerify[] =
-  Object.keys(pkg.dependencies).map(
-    (name) => {
-      return {
-        name: name,
+  // TODO check devDependencies, optionalDependencies
+  const dependenciesToCheckVersion: PackageToVerify[] = [];
+  if (rootManifest.dependencies) {
+    for (const name in rootManifest.dependencies) {
+      dependenciesToCheckVersion.push({
+        name,
         originalKey: name,
         parentCwd: registry.cwd,
-        version: pkg.dependencies[name],
-      }
+        version: rootManifest.dependencies[name],
+      });
     }
-  );
-  let locationsVisited: Set<string> = new Set();
-  while(dependenciesToCheckVersion.length) {
+  }
+
+  const locationsVisited: Set<string> = new Set();
+  while (dependenciesToCheckVersion.length) {
     const dep = dependenciesToCheckVersion.shift();
     const manifestLoc = path.join(dep.parentCwd, registry.folder, dep.name);
     if (locationsVisited.has(manifestLoc + `@${dep.version}`)) {
@@ -71,8 +70,9 @@ async function commonJsCheck(
       reportError('packageWrongVersion', dep.originalKey, dep.version, pkg.version);
       continue;
     }
-    if (pkg.dependencies) {
-      for (let subdep in pkg.dependencies) {
+    const dependencies = pkg.dependencies;
+    if (dependencies) {
+      for (const subdep in dependencies) {
         const subDepPath = path.join(manifestLoc, registry.folder, subdep);
         let found = false;
         const relative = path.relative(registry.cwd, subDepPath);
@@ -82,7 +82,8 @@ async function commonJsCheck(
         while (locations.length >= 0) {
           let possiblePath;
           if (locations.length > 0) {
-            possiblePath = path.join(registry.cwd, registry.folder, locations.join(path.sep + registry.folder + path.sep));
+            possiblePath = path.join(registry.cwd, registry.folder,
+              locations.join(path.sep + registry.folder + path.sep));
           } else {
             possiblePath = registry.cwd;
           }
@@ -91,7 +92,7 @@ async function commonJsCheck(
               name: subdep,
               originalKey: `${dep.originalKey}#${subdep}`,
               parentCwd: possiblePath,
-              version: pkg.dependencies[subdep],
+              version: dependencies[subdep],
             });
             found = true;
             break;
@@ -108,8 +109,47 @@ async function commonJsCheck(
     }
   }
 
-  if (warningCount > 1) {
-    reporter.info(reporter.lang('foundWarnings', warningCount));
+  if (errCount > 0) {
+    throw new MessageError(reporter.lang('foundErrors', errCount));
+  } else {
+    reporter.success(reporter.lang('folderInSync'));
+  }
+}
+
+async function integrityHashCheck(
+  config: Config,
+  reporter: Reporter,
+  flags: Object,
+  args: Array<string>,
+): Promise<void> {
+  let errCount = 0;
+  function reportError(msg, ...vars) {
+    reporter.error(reporter.lang(msg, ...vars));
+    errCount++;
+  }
+
+  const lockfile = await Lockfile.fromDirectory(config.cwd);
+  const install = new Install(flags, config, reporter, lockfile);
+
+  // get patterns that are installed when running `yarn install`
+  const {patterns: rawPatterns} = await install.hydrate(true);
+  const patterns = await install.flatten(rawPatterns);
+
+  // check if patterns exist in lockfile
+  for (const pattern of patterns) {
+    if (!lockfile.getLocked(pattern)) {
+      reportError('lockfileNotContainPatter', pattern);
+    }
+  }
+
+  const integrityLoc = await install.getIntegrityHashLocation();
+  if (integrityLoc && await fs.exists(integrityLoc)) {
+    const match = await install.matchesIntegrityHash(patterns);
+    if (match.matches === false) {
+      reportError('integrityHashesDontMatch', match.expected, match.actual);
+    }
+  } else {
+    reportError('noIntegirtyHashFile');
   }
 
   if (errCount > 0) {
@@ -119,23 +159,27 @@ async function commonJsCheck(
   }
 }
 
-
 export async function run(
   config: Config,
   reporter: Reporter,
   flags: Object,
   args: Array<string>,
 ): Promise<void> {
+  if (flags.commonjs) {
+    commonJsCheck(config, reporter, flags, args);
+    return;
+  } else if (flags.integrity) {
+    integrityHashCheck(config, reporter, flags, args);
+    return;
+  }
+
   let warningCount = 0;
   let errCount = 0;
   function reportError(msg, ...vars) {
     reporter.error(reporter.lang(msg, ...vars));
     errCount++;
   }
-    // TODO move to a separate subcommand
-  if (flags.commonjs) {
-    return commonJsCheck(config, reporter, flags, args);
-  }
+
   const lockfile = await Lockfile.fromDirectory(config.cwd);
   const install = new Install(flags, config, reporter, lockfile);
 
@@ -153,7 +197,6 @@ export async function run(
     }, []);
   }
 
-
   // get patterns that are installed when running `yarn install`
   const {patterns: rawPatterns} = await install.hydrate(true);
   const patterns = await install.flatten(rawPatterns);
@@ -165,137 +208,123 @@ export async function run(
     }
   }
 
-  if (flags.integrity) {
-    // just check the integrity hash for validity
-    const integrityLoc = await install.getIntegrityHashLocation();
-
-    if (integrityLoc && await fs.exists(integrityLoc)) {
-      const match = await install.matchesIntegrityHash(patterns);
-      if (match.matches === false) {
-        reportError('integrityHashesDontMatch', match.expected, match.actual);
-      }
-    } else {
-      reportError('noIntegirtyHashFile');
+  // check if any of the node_modules are out of sync
+  const res = await install.linker.getFlatHoistedTree(patterns);
+  for (const [loc, {originalKey, pkg, ignore}] of res) {
+    if (ignore) {
+      continue;
     }
-  } else {
-    // check if any of the node_modules are out of sync
-    const res = await install.linker.getFlatHoistedTree(patterns);
-    for (const [loc, {originalKey, pkg, ignore}] of res) {
-      if (ignore) {
-        continue;
-      }
 
-      const parts = humaniseLocation(loc);
+    const parts = humaniseLocation(loc);
 
-      // grey out hoisted portions of key
-      let human = originalKey;
-      const hoistedParts = parts.slice();
-      const hoistedKey = parts.join('#');
-      if (human !== hoistedKey) {
-        const humanParts = human.split('#');
+    // grey out hoisted portions of key
+    let human = originalKey;
+    const hoistedParts = parts.slice();
+    const hoistedKey = parts.join('#');
+    if (human !== hoistedKey) {
+      const humanParts = human.split('#');
 
-        for (let i = 0; i < humanParts.length; i++) {
-          const humanPart = humanParts[i];
+      for (let i = 0; i < humanParts.length; i++) {
+        const humanPart = humanParts[i];
 
-          if (hoistedParts[0] === humanPart) {
-            hoistedParts.shift();
+        if (hoistedParts[0] === humanPart) {
+          hoistedParts.shift();
 
-            if (i < humanParts.length - 1) {
-              humanParts[i] += '#';
-            }
-          } else {
-            humanParts[i] = reporter.format.dim(`${humanPart}#`);
+          if (i < humanParts.length - 1) {
+            humanParts[i] += '#';
           }
-        }
-
-        human = humanParts.join('');
-      }
-
-      const pkgLoc = path.join(loc, 'package.json');
-      if (!(await fs.exists(loc)) || !(await fs.exists(pkgLoc))) {
-        if (pkg._reference.optional) {
-          reporter.warn(reporter.lang('optionalDepNotInstalled', human));
         } else {
-          reportError('packageNotInstalled', human);
+          humanParts[i] = reporter.format.dim(`${humanPart}#`);
         }
-        continue;
       }
 
-      const packageJson = await config.readJson(pkgLoc);
-      if (pkg.version !== packageJson.version) {
-        // node_modules contains wrong version
-        reportError('packageWrongVersion', human, pkg.version, packageJson.version);
+      human = humanParts.join('');
+    }
+
+    const pkgLoc = path.join(loc, 'package.json');
+    if (!(await fs.exists(loc)) || !(await fs.exists(pkgLoc))) {
+      if (pkg._reference.optional) {
+        reporter.warn(reporter.lang('optionalDepNotInstalled', human));
+      } else {
+        reportError('packageNotInstalled', human);
+      }
+      continue;
+    }
+
+    const packageJson = await config.readJson(pkgLoc);
+    if (pkg.version !== packageJson.version) {
+      // node_modules contains wrong version
+      reportError('packageWrongVersion', human, pkg.version, packageJson.version);
+    }
+
+    const deps = Object.assign({}, packageJson.dependencies, packageJson.peerDependencies);
+
+    for (const name in deps) {
+      const range = deps[name];
+      if (!semver.validRange(range, config.looseSemver)) {
+        continue; // exotic
       }
 
-      const deps = Object.assign({}, packageJson.dependencies, packageJson.peerDependencies);
+      const subHuman = `${human}#${name}@${range}`;
 
-      for (const name in deps) {
-        const range = deps[name];
-        if (!semver.validRange(range, config.looseSemver)) {
-          continue; // exotic
-        }
+      // find the package that this will resolve to, factoring in hoisting
+      const possibles = [];
+      let depPkgLoc;
+      for (let i = parts.length; i >= 0; i--) {
+        const myParts = parts.slice(0, i).concat(name);
 
-        const subHuman = `${human}#${name}@${range}`;
+        // build package.json location for this position
+        const myDepPkgLoc = path.join(
+          config.cwd,
+          'node_modules',
+          myParts.join(`${path.sep}node_modules${path.sep}`),
+          'package.json',
+        );
 
-        // find the package that this will resolve to, factoring in hoisting
-        const possibles = [];
-        let depPkgLoc;
-        for (let i = parts.length; i >= 0; i--) {
-          const myParts = parts.slice(0, i).concat(name);
-
-          // build package.json location for this position
-          const myDepPkgLoc = path.join(
-            config.cwd,
-            'node_modules',
-            myParts.join(`${path.sep}node_modules${path.sep}`),
-            'package.json',
-          );
-
-          possibles.push(myDepPkgLoc);
-        }
-        while (possibles.length) {
-          const myDepPkgLoc = possibles.shift();
-          if (await fs.exists(myDepPkgLoc)) {
-            depPkgLoc = myDepPkgLoc;
-            break;
-          }
-        }
-        if (!depPkgLoc) {
-          // we'll hit the module not install error above when this module is hit
-          continue;
-        }
-
-        //
-        const depPkg = await config.readJson(depPkgLoc);
-        const foundHuman = `${humaniseLocation(path.dirname(depPkgLoc)).join('#')}@${depPkg.version}`;
-        if (!semver.satisfies(depPkg.version, range, config.looseSemver)) {
-          // module isn't correct semver
-          reportError('packageDontSatisfy', subHuman, foundHuman);
-          continue;
-        }
-
-        // check for modules above us that this could be deduped to
-        for (const loc of possibles) {
-          if (!await fs.exists(loc)) {
-            continue;
-          }
-
-          const packageJson = await config.readJson(loc);
-          if (packageJson.version === depPkg.version ||
-             (semver.satisfies(packageJson.version, range, config.looseSemver) &&
-             semver.gt(packageJson.version, depPkg.version, config.looseSemver))) {
-            reporter.warn(
-              reporter.lang(
-                'couldBeDeduped',
-                subHuman,
-                packageJson.version,
-                `${humaniseLocation(path.dirname(loc)).join('#')}@${packageJson.version}`,
-              ),
-            );
-            warningCount++;
-          }
+        possibles.push(myDepPkgLoc);
+      }
+      while (possibles.length) {
+        const myDepPkgLoc = possibles.shift();
+        if (await fs.exists(myDepPkgLoc)) {
+          depPkgLoc = myDepPkgLoc;
           break;
         }
+      }
+      if (!depPkgLoc) {
+        // we'll hit the module not install error above when this module is hit
+        continue;
+      }
+
+      //
+      const depPkg = await config.readJson(depPkgLoc);
+      const foundHuman = `${humaniseLocation(path.dirname(depPkgLoc)).join('#')}@${depPkg.version}`;
+      if (!semver.satisfies(depPkg.version, range, config.looseSemver)) {
+        // module isn't correct semver
+        reportError('packageDontSatisfy', subHuman, foundHuman);
+        continue;
+      }
+
+      // check for modules above us that this could be deduped to
+      for (const loc of possibles) {
+        if (!await fs.exists(loc)) {
+          continue;
+        }
+
+        const packageJson = await config.readJson(loc);
+        if (packageJson.version === depPkg.version ||
+           (semver.satisfies(packageJson.version, range, config.looseSemver) &&
+           semver.gt(packageJson.version, depPkg.version, config.looseSemver))) {
+          reporter.warn(
+            reporter.lang(
+              'couldBeDeduped',
+              subHuman,
+              packageJson.version,
+              `${humaniseLocation(path.dirname(loc)).join('#')}@${packageJson.version}`,
+            ),
+          );
+          warningCount++;
+        }
+        break;
       }
     }
   }
