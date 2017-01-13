6c9d2971d3f80d6eeeb2177c9fa9ccb401a57791
Philipp Zins <philipp.zins@mercateo.com>
2017-01-13 08:56:23 +0100

    added failing unit test for #2165

--- __tests__/commands/install/integration.js
+++ __tests__/commands/install/integration.js
@@ -212,6 +212,26 @@ test.concurrent('install file: protocol with relative paths', (): Promise<void> 
   });
 });
 
+test.concurrent('install file: protocol without cache', async (): Promise<void> => {
+  const fixturesLoc = path.join(__dirname, '..', '..', 'fixtures', 'install');
+  const compLoc = path.join(fixturesLoc, 'install-file-without-cache', 'comp', 'index.js');
+
+  return fs.writeFile(compLoc,'foo\n').then(() =>
+    runInstall({noLockfile: true}, 'install-file-without-cache', async (config) => {
+      assert.equal(
+        await fs.readFile(path.join(config.cwd, 'node_modules', 'comp', 'index.js')),
+        'foo\n',
+      );
+      await fs.writeFile(compLoc, 'bar\n');
+      await runInstall({noLockfile: true}, 'install-file-without-cache');
+      assert.equal(
+        await fs.readFile(path.join(config.cwd, 'node_modules', 'comp', 'index.js')),
+        'bar\n',
+      );
+    })
+  );
+});
+
 test.concurrent('install file: protocol', (): Promise<void> => {
   return runInstall({noLockfile: true}, 'install-file', async (config) => {
     assert.equal(
