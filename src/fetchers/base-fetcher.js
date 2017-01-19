8d4c7b2d7fc8447bb6195aa8ed5b5e557eb809bc
Wei Wei <lyweiwei@outlook.com>
2017-01-20 03:46:20 +0800

    Fix flow check

--- src/fetchers/base-fetcher.js
+++ src/fetchers/base-fetcher.js
@@ -25,6 +25,7 @@ export default class BaseFetcher {
   reporter: Reporter;
   remote: PackageRemote;
   registry: RegistryNames;
+  packageName: ?string;
   reference: string;
   config: Config;
   hash: ?string;
