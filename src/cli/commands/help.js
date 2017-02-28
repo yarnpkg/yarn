6e383ff20196c45851be7e396e4679970082af4b
Simon Vocella <voxsim@gmail.com>
2017-02-28 22:25:42 +0100

    make help command a class

--- src/cli/commands/help.js
+++ src/cli/commands/help.js
@@ -7,38 +7,40 @@ import type Config from '../../config.js';
 import {sortAlpha, hyphenate} from '../../util/misc.js';
 const chalk = require('chalk');
 
-export function hasWrapper(): boolean {
-  return false;
-}
-
-export function run(
-  config: Config,
-  reporter: Reporter,
-  commander: Object,
-  args: Array<string>,
-): Promise<void> {
-  const getDocsLink = (name) => `${constants.YARN_DOCS}${name || ''}`;
-  const getDocsInfo = (name) => 'Visit ' + chalk.bold(getDocsLink(name)) + ' for documentation about this command.';
-
-  if (args.length) {
-    const helpCommand = hyphenate(args[0]);
-    if (commands[helpCommand]) {
-      commander.on('--help', () => console.log('  ' + getDocsInfo(helpCommand) + '\n'));
-    }
-  } else {
-    commander.on('--help', () => {
-      console.log('  Commands:\n');
-      for (const name of Object.keys(commands).sort(sortAlpha)) {
-        if (commands[name].useless) {
-          continue;
-        }
-
-        console.log(`    - ${hyphenate(name)}`);
-      }
-      console.log('\n  Run `' + chalk.bold('yarn help COMMAND') + '` for more information on specific commands.');
-      console.log('  Visit ' + chalk.bold(getDocsLink()) + ' to learn more about Yarn.\n');
-    });
+export default class HelpCommand {
+  static hasWrapper(): boolean {
+    return false;
   }
-  commander.help();
-  return Promise.resolve();
+
+  static run(
+    config: Config,
+    reporter: Reporter,
+    commander: Object,
+    args: Array<string>,
+  ): Promise<void> {
+    const getDocsLink = (name) => `${constants.YARN_DOCS}${name || ''}`;
+    const getDocsInfo = (name) => 'Visit ' + chalk.bold(getDocsLink(name)) + ' for documentation about this command.';
+
+    if (args.length) {
+      const helpCommand = hyphenate(args[0]);
+      if (commands[helpCommand]) {
+        commander.on('--help', () => console.log('  ' + getDocsInfo(helpCommand) + '\n'));
+      }
+    } else {
+      commander.on('--help', () => {
+        console.log('  Commands:\n');
+        for (const name of Object.keys(commands).sort(sortAlpha)) {
+          if (commands[name].useless) {
+            continue;
+          }
+
+          console.log(`    - ${hyphenate(name)}`);
+        }
+        console.log('\n  Run `' + chalk.bold('yarn help COMMAND') + '` for more information on specific commands.');
+        console.log('  Visit ' + chalk.bold(getDocsLink()) + ' to learn more about Yarn.\n');
+      });
+    }
+    commander.help();
+    return Promise.resolve();
+  }
 }
