2f49dcfe0119a960e28958679be15ace5c527ca3
HOU Bin <kouhin@users.noreply.github.com>
2017-03-08 00:25:13 +0900

    use process.mainModule.filename as npm_execpath (#2843)

--- src/util/execute-lifecycle-script.js
+++ src/util/execute-lifecycle-script.js
@@ -30,7 +30,7 @@ async function makeEnv(stage: string, cwd: string, config: Config): {
 
   env.npm_lifecycle_event = stage;
   env.npm_node_execpath = env.NODE || process.execPath;
-  env.npm_execpath = path.join(__dirname, '..', '..', 'bin', 'yarn.js');
+  env.npm_execpath = env.npm_execpath || process.mainModule.filename;
 
   // Set the env to production for npm compat if production mode.
   // https://github.com/npm/npm/blob/30d75e738b9cb7a6a3f9b50e971adcbe63458ed3/lib/utils/lifecycle.js#L336
