3bcbd4d4cb32558653e1d9736f77608b3cfa41f2
Cédric Connes <cedric.connes@gmail.com>
2017-03-07 16:05:50 +0100

    Move ROOT_USER to a separate file, fixes #2807 (#2857)

--- src/util/user-home-dir.js
+++ src/util/user-home-dir.js
@@ -1,6 +1,8 @@
 /* @flow */
+
+import ROOT_USER from './root-user.js';
+
 const path = require('path');
-const {ROOT_USER} = require('../constants');
 
 const userHomeDir = (process.platform === 'linux' && ROOT_USER) ?
   path.resolve('/usr/local/share') :
