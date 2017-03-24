6fa8732f7e4c979819598b66ca579ad89eaa5944
Konstantin Raev <bestander@gmail.com>
2017-03-24 12:29:21 +0000

    Mock inquirer for all e2e tests (#2982)
    * Mock inquirer for all e2e tests
    
    Fixes #2955, we don't want yarn to prompt during e2e installs
    
    * fix exports
    
    * fixing test

---
+++ __tests__/__mocks__/inquirer.js
@@ -0,0 +1,10 @@
+/* @flow */
+
+const realInquirer = (require: any).requireActual('inquirer');
+
+realInquirer.prompt = jest.fn((questions) => {
+  const chosenVersion = questions[0].choices[0];
+  return Promise.resolve({package: chosenVersion});
+});
+
+module.exports = realInquirer;
