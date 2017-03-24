2b97367a0b73461840d216915f7528280e76eb57
Konstantin Raev <bestander@fb.com>
2017-03-24 11:29:16 +0000

    fix exports

--- __tests__/__mocks__/inquirer.js
+++ __tests__/__mocks__/inquirer.js
@@ -6,3 +6,5 @@ realInquirer.prompt = jest.fn((questions) => {
   const chosenVersion = questions[0].choices[0];
   return Promise.resolve({package: chosenVersion});
 });
+
+module.exports = realInquirer;
