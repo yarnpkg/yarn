372afd5d2d01b997c210d71a265b87381892adad
Maël Nison <mael@fb.com>
2017-04-06 13:05:26 +0100

    Fixes test on Windows

--- __tests__/lifecycle-scripts.js
+++ __tests__/lifecycle-scripts.js
@@ -32,13 +32,13 @@ async function execCommand(cmd: string, packageName: string, env = process.env):
 
 test('should add the yarnrc values to the command line', async () => {
   const stdout = await execCommand('cache dir', 'yarnrc-cli');
-  expect(stdout).toMatch(/^\/tmp\/foobar\/v[0-9]+\n$/);
+  expect(stdout.replace(/\\/g, '/')).toMatch(/^\/tmp\/foobar\/v[0-9]+\n$/);
 });
 
 // Test disabled for now, cf rc.js
 test.skip('should resolve the yarnrc values relative to where the file lives', async () => {
   const stdout = await execCommand('cache dir', 'yarnrc-cli-relative');
-  expect(stdout).toMatch(/^(\/[^\/]+)+\/foobar\/hello\/world\/v[0-9]+\n$/);
+  expect(stdout.replace(/\\/g, '/')).toMatch(/^(\/[^\/]+)+\/foobar\/hello\/world\/v[0-9]+\n$/);
 });
 
 test('should expose `npm_config_argv` environment variable to lifecycle scripts for back compatibility with npm (#684)',
