yarnpkg/yarn

commit 9316ebd62750d0dd7e68325511bb228bcf9e7e5e

Set boolean config values correctly (#2440)
* Fixes #2434

src/cli/commands/config.js
@@ -19,8 +19,8 @@ export const {run, setFlags} = buildSubCommands('config', {
     if (args.length === 0 || args.length > 2) {
       return false;
     }
-
-    const [key, val = true] = args;
+    const key = args[0];
+    const val = args[1] || true;
     const yarnConfig = config.registries.yarn;
     await yarnConfig.saveHomeConfig({[key]: val});
     reporter.success(reporter.lang('configSet', key, val));

__tests__/commands/config.js
@@ -31,3 +31,21 @@ test('cache-folder flag has higher priorities than .yarnrc file', (): Promise<vo
       expect(config.cacheFolder).toContain('flag_config_folder_dir');
     });
 });
+
+test('set true when option value is empty', (): Promise<void> => {
+  return runConfig(['set', 'strict-ssl', ''], {}, '', (config) => {
+    expect(config.registries.yarn.homeConfig['strict-ssl']).toBe(true);
+  });
+});
+
+test('set value "false" to an option', (): Promise<void> => {
+  return runConfig(['set', 'strict-ssl', 'false'], {}, '', (config) => {
+    expect(config.registries.yarn.homeConfig['strict-ssl']).toBe(false);
+  });
+});
+
+test('set value "true" to an option', (): Promise<void> => {
+  return runConfig(['set', 'strict-ssl', 'true'], {}, '', (config) => {
+    expect(config.registries.yarn.homeConfig['strict-ssl']).toBe(true);
+  });
+});

src/registries/yarn-registry.js
@@ -101,6 +101,8 @@ export default class YarnRegistry extends NpmRegistry {
   }
 
   async saveHomeConfig(config: Object): Promise<void> {
+    YarnRegistry.normalizeConfig(config);
+
     for (const key in config) {
       const val = config[key];
 

commit d36b93a5218b4ecc80ca092412bf97dec9205a89

Fix bug when cannot set a config value to empty string
* Fixes regression introduced in PR #2440
* Related to #2434

src/cli/commands/config.js
@@ -19,8 +19,7 @@ export const {run, setFlags} = buildSubCommands('config', {
     if (args.length === 0 || args.length > 2) {
       return false;
     }
-    const key = args[0];
-    const val = args[1] || true;
+    const [key, val = true] = args;
     const yarnConfig = config.registries.yarn;
     await yarnConfig.saveHomeConfig({[key]: val});
     reporter.success(reporter.lang('configSet', key, val));

__tests__/commands/config.js
@@ -32,9 +32,15 @@ test('cache-folder flag has higher priorities than .yarnrc file', (): Promise<vo
     });
 });
 
-test('set true when option value is empty', (): Promise<void> => {
-  return runConfig(['set', 'strict-ssl', ''], {}, '', (config) => {
+test('set true when option value is undefined', (): Promise<void> => {
+  return runConfig(['set', 'strict-ssl'], {}, '', (config) => {
     expect(config.registries.yarn.homeConfig['strict-ssl']).toBe(true);
+  });
+});
+
+test('set empty string to an option', (): Promise<void> => {
+  return runConfig(['set', 'version-tag-prefix', ''], {}, '', (config) => {
+    expect(config.registries.yarn.homeConfig['version-tag-prefix']).toBe('');
   });
 });
 