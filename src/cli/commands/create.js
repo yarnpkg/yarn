6646ef7e632ad4c1f6c52e8c70ee211c3caa7e03
Maël Nison <mael@fb.com>
2017-05-02 15:09:44 +0100

    Fix stdio redirection

--- src/cli/commands/create.js
+++ src/cli/commands/create.js
@@ -28,5 +28,5 @@ export async function run(
   }
 
   await runGlobal(config, reporter, {}, [ 'add', `yarn-create-${args[0]}` ]);
-  await spawn(`${getGlobalBinFolder(config, flags)}/yarn-create-${args[0]}`, []);
+  await spawn(`${getGlobalBinFolder(config, flags)}/yarn-create-${args[0]}`, [], { stdio: `inherit` });
 }
