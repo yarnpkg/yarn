bbfa1315f630a27fb825cb58c19facc444fb8774
Maël Nison <mael@fb.com>
2017-05-18 14:22:17 +0200

    Disables the hash check for git repositories

--- src/fetchers/git-fetcher.js
+++ src/fetchers/git-fetcher.js
@@ -91,15 +91,20 @@ export default class GitFetcher extends BaseFetcher {
         .pipe(hashStream)
         .pipe(untarStream)
         .on('finish', () => {
+
           const expectHash = this.hash;
           const actualHash = hashStream.getHash();
-          if (!expectHash || expectHash === actualHash) {
+
+          // This condition is disabled because "expectHash" actually is the commit hash
+          // This is a design issue that we'll need to fix (https://github.com/yarnpkg/yarn/pull/3449)
+          if (true || !expectHash || expectHash === actualHash) {
             resolve({
               hash: actualHash,
             });
           } else {
             reject(new SecurityError(this.reporter.lang('fetchBadHash', expectHash, actualHash)));
           }
+
         })
         .on('error', function(err) {
           reject(new MessageError(this.reporter.lang('fetchErrorCorrupt', err.message, tarballPath)));
