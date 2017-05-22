4759d6bb16797b1fd5a692d7b7efd4090a9792ea
battaile <battaile@gmail.com>
2017-05-22 16:11:24 -0400

    Fix npm command
    'update' instead of 'upgrade'

--- src/cli/commands/install.js
+++ src/cli/commands/install.js
@@ -85,7 +85,7 @@ function getUpdateCommand(installationMethod: InstallationMethod): ?string {
   }
 
   if (installationMethod === 'npm') {
-    return 'npm upgrade --global yarn';
+    return 'npm update --global yarn';
   }
 
   if (installationMethod === 'chocolatey') {
