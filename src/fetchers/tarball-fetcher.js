9212f4b3eab5fe50e7f46ca4c9bc1388fee4a2ee
Maël Nison <mael@fb.com>
2017-05-23 18:01:16 +0100

    Fixes linting

--- src/fetchers/tarball-fetcher.js
+++ src/fetchers/tarball-fetcher.js
@@ -116,7 +116,7 @@ export default class TarballFetcher extends BaseFetcher {
       const {validateStream, extractorStream} = this.createExtractor(resolve, reject);
       const cachedStream = fs.createReadStream(tarballPath);
 
-      cachedStream.pipe(validateStream).pipe(extractorStream).on('error', err => {
+      cachedStream.pipe(validateStream).pipe(extractorStream).on('error', (err) => {
         reject(new MessageError(this.config.reporter.lang('fetchErrorCorrupt', err.message, tarballPath)));
       });
     });
