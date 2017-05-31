57d527dd8b4ac36eb93da1afae6bfc27d8e32fbd
Maël Nison <mael@fb.com>
2017-05-31 15:23:10 +0100

    Calls setupMirrorFromCache when fetching through the git-fetcher

--- src/fetchers/git-fetcher.js
+++ src/fetchers/git-fetcher.js
@@ -16,6 +16,20 @@ const fs = require('fs');
 const invariant = require('invariant');
 
 export default class GitFetcher extends BaseFetcher {
+  async setupMirrorFromCache(): Promise<?string> {
+    const tarballMirrorPath = this.getTarballMirrorPath();
+    const tarballCachePath = this.getTarballCachePath();
+
+    if (tarballMirrorPath == null) {
+      return;
+    }
+
+    if (!await fsUtil.exists(tarballMirrorPath) && (await fsUtil.exists(tarballCachePath))) {
+      // The tarball doesn't exists in the offline cache but does in the cache; we import it to the mirror
+      await fsUtil.copy(tarballCachePath, tarballMirrorPath, this.reporter);
+    }
+  }
+
   async getLocalAvailabilityStatus(): Promise<boolean> {
     // Some mirrors might still have files named "./reponame" instead of "./reponame-commit"
     const tarballLegacyMirrorPath = this.getTarballMirrorPath({
