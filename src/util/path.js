0c948ede95458fd7130581b797bed4d51536cca9
Maël Nison <mael@fb.com>
2017-06-05 16:25:43 +0100

    Adds support for both slashs and backslashes on Windows

--- src/util/path.js
+++ src/util/path.js
@@ -2,6 +2,10 @@
 
 const userHome = require('./user-home-dir').default;
 
+export function getPosixPath(path: string): string {
+  return path.replace(/\\/g, '/');
+}
+
 export function expandPath(path: string): string {
   if (process.platform !== 'win32') {
     path = path.replace(/^\s*~(?=$|\/|\\)/, userHome);
