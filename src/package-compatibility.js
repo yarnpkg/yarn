879b4f3c893b5e5ec162a1d0a9234c3793e7ac01
Onur Temizkan <onurtemizkan@gmail.com>
2017-07-11 20:49:52 +0300

    Lower platform incompatibility message to info (#3902)

--- src/package-compatibility.js
+++ src/package-compatibility.js
@@ -110,7 +110,7 @@ export function checkOne(info: Manifest, config: Config, ignoreEngines: boolean)
       ref.ignore = true;
       ref.incompatible = true;
 
-      reporter.warn(`${human}: ${msg}`);
+      reporter.info(`${human}: ${msg}`);
       if (!didIgnore) {
         reporter.info(reporter.lang('optionalCompatibilityExcluded', human));
         didIgnore = true;
