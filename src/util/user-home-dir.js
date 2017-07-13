02701bcb0773ae43d93bc85984fff3f697e0158f
Richard Weber <riche.weber@gmail.com>
2017-07-13 10:35:45 +0300

    Do not use /usr/local/share as home under fakeroot
    Fixex #2536

--- src/util/user-home-dir.js
+++ src/util/user-home-dir.js
@@ -4,11 +4,7 @@ import ROOT_USER from './root-user.js';
 
 const path = require('path');
 
-const isNotFakeRoot = Boolean(process.env.FAKEROOTKEY);
-
 const userHomeDir =
-  process.platform === 'linux' && ROOT_USER && isNotFakeRoot
-    ? path.resolve('/usr/local/share')
-    : require('os').homedir();
+  process.platform === 'linux' && ROOT_USER ? path.resolve('/usr/local/share') : require('os').homedir();
 
 export default userHomeDir;
