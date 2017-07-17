e7eac56cdcb210b13da417f390bd3f331f40bcbc
Daniel Lo Nigro <daniel@dan.cx>
2017-07-17 03:22:12 -0700

    Publish npm release of Yarn directly from CircleCI (#3938)

--- scripts/update-npm.sh
+++ scripts/update-npm.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-# Pushes the latest Yarn version to npm
+# Pushes the locally built Yarn version to npm
 
 set -ex
 
-# See if YARN_VERSION was passed in the environment, otherwise get version
-# number from Yarn site
-if [ -z "$YARN_VERSION" ]; then
-  echo 'Getting Yarn version from https://yarnpkg.com/latest-version'
-  version=`curl --fail https://yarnpkg.com/latest-version`
-else
-  version="$YARN_VERSION"
-fi
+version=`./dist/bin/yarn --version`
+tarball="./artifacts/yarn-v$version.tar.gz"
+
+# Ensure Yarn tarball was built
+if [ ! -f "$tarball" ]; then
+  echo 'Could not find Yarn tarball; please re-run "yarn build-dist".'
+  exit 1
+fi;
 
 # Check if this version is already published to npm.
 # Note: npm doesn't return a non-zero error code when "npm view" can't find a
@@ -20,14 +20,11 @@ if [ "`npm view yarn@$version name`" = 'yarn' ]; then
   exit 0
 fi;
 
-# Grab the tarball so we can publish it as-is
-url=https://yarnpkg.com/downloads/$version/yarn-v$version.tar.gz
-tempfile=`mktemp -t 'yarn-release-XXXXXXXX.tar.gz'`
-curl --fail -L -o $tempfile $url
-
+# Determine if this is an RC or a stable release
+release_type=`curl --fail https://release.yarnpkg.com/release_type/$version`
 npm_flags=''
-if [ "$YARN_RC" = "true" ]; then
+if [ "$release_type" = "rc" ]; then
   npm_flags='--tag rc'
 fi;
 
-eval "npm publish '$tempfile' --access public $npm_flags"
+eval "npm publish '$tarball' --access public $npm_flags"
