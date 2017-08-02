462fbd24448d1ad27acbca1cfac3b298a88c5417
Burak Yiğit Kaya <ben@byk.im>
2017-08-02 16:19:14 +0100

    Fix: Don't run pre/post hooks if they are not scripts (#4068)
    **Summary**
    
    Fixes #3994. We should not run installed binaries if
    they correspond to `pre/post` script names. Similarly,
    `pre/post` scripts should not be run if a binary is run.
    
    **Test plan**
    
    Added 2 new tests.

--- src/cli/commands/run.js
+++ src/cli/commands/run.js
@@ -52,20 +52,29 @@ export async function run(config: Config, reporter: Reporter, flags: Object, arg
       cmdHints[cmd] = pkgScripts[cmd] || '';
     }
 
-    Object.assign(scripts, pkg.scripts);
+    Object.assign(scripts, pkgScripts);
   }
 
   async function runCommand(args): Promise<void> {
     const action = args.shift();
-    const actions = [`pre${action}`, action, `post${action}`];
 
     // build up list of commands
     const cmds = [];
-    for (const action of actions) {
-      const cmd = scripts[action];
-      if (cmd) {
-        cmds.push([action, cmd]);
+
+    if (pkgScripts && action in pkgScripts) {
+      const preAction = `pre${action}`;
+      if (preAction in pkgScripts) {
+        cmds.push([preAction, pkgScripts[preAction]]);
       }
+
+      cmds.push([action, scripts[action]]);
+
+      const postAction = `post${action}`;
+      if (postAction in pkgScripts) {
+        cmds.push([postAction, pkgScripts[postAction]]);
+      }
+    } else if (scripts[action]) {
+      cmds.push([action, scripts[action]]);
     }
 
     if (cmds.length) {
