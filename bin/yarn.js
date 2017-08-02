2c54c02453341a59f29dcd05e657edd482b7c779
Burak Yiğit Kaya <ben@byk.im>
2017-08-02 12:54:40 +0100

    Fix: Fix single-file builds and simplify bundles (#4063)
    **Summary**
    
    Fixes #4057.
    
    **Test plan**
    
    CI should pass, especially with `build-dist` job.
    
    Manual:
    
     - Run `yarn build-dist`
     - Run `node artifacts/yarn-0.28.0.js --version`
     - Run `./dist/bin/yarn --version`
    
    Make sure all above commands work without errors.

--- bin/yarn.js
+++ bin/yarn.js
@@ -4,41 +4,26 @@
 /* eslint-disable flowtype/require-valid-file-annotation */
 'use strict';
 
-// validate that used node version is supported
-var semver = require('semver');
 var ver = process.versions.node;
-ver = ver.split('-')[0]; // explode and truncate tag from version #511
+var majorVer = parseInt(ver.split('.')[0], 10);
 
-var dirPath = null;
-
-if (semver.satisfies(ver, '>=5.0.0')) {
-  dirPath = '../lib';
-} else if (semver.satisfies(ver, '>=4.0.0')) {
-  dirPath = '../lib-legacy';
+if (majorVer < 4) {
+  console.error('Node version ' + ver + ' is not supported, please use Node.js 4.0 or higher.');
+  process.exitCode = 1;
 } else {
-  console.log(require('chalk').red('Node version ' + ver + ' is not supported, please use Node.js 4.0 or higher.'));
-  // We need to skip the rest and terminate here since the Node version is not supported.
-  // `console.log` calls are sync so we don't have a risk of exiting early before stdout flushes
-  process.exit(1); // eslint-disable-line no-process-exit
+  var dirPath = '../lib/';
+  var v8CompileCachePath = dirPath + 'v8-compile-cache';
+  var fs = require('fs');
+  // We don't have/need this on legacy builds and dev builds
+  if (fs.existsSync(v8CompileCachePath)) {
+    require(v8CompileCachePath);
+  }
+
+  // Just requiring this package will trigger a yarn run since the
+  // `require.main === module` check inside `cli/index.js` will always
+  // be truthy when built with webpack :(
+  var cli = require(dirPath + 'cli');
+  if (!cli.autoRun) {
+    cli.default();
+  }
 }
-
-// load v8-compile-cache
-if (semver.satisfies(ver, '>=5.7.0')) {
-  require('v8-compile-cache');
-}
-
-// ensure cache directory exists
-var mkdirp = require('mkdirp');
-var constants = require(dirPath + '/constants');
-mkdirp.sync(constants.MODULE_CACHE_DIRECTORY);
-
-module.exports = require(dirPath + '/cli');
-const main = module.exports.default;
-
-// ignore all arguments after a --
-const doubleDashIndex = process.argv.findIndex(element => element === '--');
-const startArgs = process.argv.slice(0, 2);
-const args = process.argv.slice(2, doubleDashIndex === -1 ? process.argv.length : doubleDashIndex);
-const endArgs = doubleDashIndex === -1 ? [] : process.argv.slice(doubleDashIndex + 1, process.argv.length);
-
-main({startArgs, args, endArgs});
