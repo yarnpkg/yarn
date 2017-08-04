9131fca15ed495ae4f33bd8954be72a9a7b11565
Burak Yigit Kaya <byk@fb.com>
2017-08-04 13:17:16 +0100

    Fix build-deb version checking

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -13,7 +13,7 @@ ensureAvailable lintian
 ensureAvailable rpmbuild
 
 PACKAGE_TMPDIR=tmp/debian_pkg
-VERSION=`dist/bin/yarn --version`
+VERSION=`./artifacts/yarn-legacy-* --version`
 OUTPUT_DIR=artifacts
 TARBALL_NAME=$OUTPUT_DIR/yarn-v$VERSION.tar.gz
 DEB_PACKAGE_NAME=yarn_$VERSION'_all.deb'
