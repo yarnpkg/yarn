03b193acf5007de2e24729fbf22a210778376343
nick_olinger <olingern@gmail.com>
2017-08-03 23:34:21 -0400

    Implement list filtering based upon NODE_ENV
    load devDeps from manifest and filter if production
    
    add constants for node env and dont pass in env to getEnv
    
    add tests for NODE_ENV_DEV, NODE_ENV_PROD, getEnv, and isProduction
    
    remove extra line break
    
    Test list output dependent upon environment
    
    rename pkgPatterns
    
    update list test with snapshot and use local pkg

--- __tests__/commands/list.js
+++ __tests__/commands/list.js
@@ -25,115 +25,145 @@ const runList = buildRun.bind(null, BufferReporter, fixturesLoc, (args, flags, c
   return list(config, reporter, flags, args);
 });
 
-test.concurrent('throws if lockfile out of date', (): Promise<void> => {
-  const reporter = new reporters.ConsoleReporter({});
+describe('list', () => {
+  test.concurrent('throws if lockfile out of date', (): Promise<void> => {
+    const reporter = new reporters.ConsoleReporter({});
 
-  return new Promise(async resolve => {
-    try {
-      await runList([], {}, 'lockfile-outdated');
-    } catch (err) {
-      expect(err.message).toContain(reporter.lang('lockfileOutdated'));
-    } finally {
-      resolve();
-    }
+    return new Promise(async resolve => {
+      try {
+        await runList([], {}, 'lockfile-outdated');
+      } catch (err) {
+        expect(err.message).toContain(reporter.lang('lockfileOutdated'));
+      } finally {
+        resolve();
+      }
+    });
   });
-});
 
-test.concurrent('lists everything with no args', (): Promise<void> => {
-  return runList([], {}, 'no-args', (config, reporter): ?Promise<void> => {
-    const rprtr = new BufferReporter({});
-    const tree = reporter.getBuffer().slice(-1);
-    const children = [{name: 'is-plain-obj@^1.0.0', color: 'dim', shadow: true}];
-    const trees = [
-      makeTree('left-pad@1.1.3', {color: 'bold'}),
-      makeTree('sort-keys@1.1.2', {children, color: 'bold'}),
-      makeTree('is-plain-obj@1.1.0'),
-    ];
+  test.concurrent('lists everything with no args', (): Promise<void> => {
+    return runList([], {}, 'no-args', (config, reporter): ?Promise<void> => {
+      const rprtr = new BufferReporter({});
+      const tree = reporter.getBuffer().slice(-1);
+      const children = [{name: 'is-plain-obj@^1.0.0', color: 'dim', shadow: true}];
+      const trees = [
+        makeTree('left-pad@1.1.3', {color: 'bold'}),
+        makeTree('sort-keys@1.1.2', {children, color: 'bold'}),
+        makeTree('is-plain-obj@1.1.0'),
+      ];
 
-    rprtr.tree('list', trees);
+      rprtr.tree('list', trees);
 
-    expect(tree).toEqual(rprtr.getBuffer());
+      expect(tree).toEqual(rprtr.getBuffer());
+    });
   });
-});
 
-test.concurrent('respects depth flag', (): Promise<void> => {
-  return runList([], {depth: 1}, 'depth-flag', (config, reporter): ?Promise<void> => {
-    const rprtr = new BufferReporter({});
-    const tree = reporter.getBuffer().slice(-1);
-    const trees = [makeTree('sort-keys@1.1.2', {color: 'bold'}), makeTree('is-plain-obj@1.1.0')];
+  test.concurrent('respects depth flag', (): Promise<void> => {
+    return runList([], {depth: 1}, 'depth-flag', (config, reporter): ?Promise<void> => {
+      const rprtr = new BufferReporter({});
+      const tree = reporter.getBuffer().slice(-1);
+      const trees = [makeTree('sort-keys@1.1.2', {color: 'bold'}), makeTree('is-plain-obj@1.1.0')];
 
-    rprtr.tree('list', trees);
+      rprtr.tree('list', trees);
 
-    expect(tree).toEqual(rprtr.getBuffer());
+      expect(tree).toEqual(rprtr.getBuffer());
+    });
   });
-});
 
-test.concurrent('accepts an argument', (): Promise<void> => {
-  return runList(['is-plain-obj'], {}, 'one-arg', (config, reporter): ?Promise<void> => {
-    const rprtr = new BufferReporter({});
-    const tree = reporter.getBuffer().slice(-1);
-    const trees = [makeTree('is-plain-obj@1.1.0')];
+  test.concurrent('accepts an argument', (): Promise<void> => {
+    return runList(['is-plain-obj'], {}, 'one-arg', (config, reporter): ?Promise<void> => {
+      const rprtr = new BufferReporter({});
+      const tree = reporter.getBuffer().slice(-1);
+      const trees = [makeTree('is-plain-obj@1.1.0')];
 
-    rprtr.tree('list', trees);
+      rprtr.tree('list', trees);
 
-    expect(tree).toEqual(rprtr.getBuffer());
+      expect(tree).toEqual(rprtr.getBuffer());
+    });
   });
-});
 
-test.concurrent('matches exactly without glob', (): Promise<void> => {
-  return runList(['gulp'], {}, 'glob-arg', (config, reporter): ?Promise<void> => {
-    const rprtr = new BufferReporter({});
-    const tree = reporter.getBuffer().slice(-1);
-    const trees = [makeTree('gulp@3.9.1', {color: 'bold'})];
+  test.concurrent('matches exactly without glob', (): Promise<void> => {
+    return runList(['gulp'], {}, 'glob-arg', (config, reporter): ?Promise<void> => {
+      const rprtr = new BufferReporter({});
+      const tree = reporter.getBuffer().slice(-1);
+      const trees = [makeTree('gulp@3.9.1', {color: 'bold'})];
 
-    rprtr.tree('list', trees);
+      rprtr.tree('list', trees);
 
-    expect(tree).toEqual(rprtr.getBuffer());
+      expect(tree).toEqual(rprtr.getBuffer());
+    });
   });
-});
 
-test.concurrent('expands a glob', (): Promise<void> => {
-  return runList(['gulp*'], {}, 'glob-arg', (config, reporter): ?Promise<void> => {
-    const rprtr = new BufferReporter({});
-    const tree = reporter.getBuffer().slice(-1);
-    const trees = [makeTree('gulp@3.9.1', {color: 'bold'}), makeTree('gulp-babel@6.1.2', {color: 'bold'})];
+  test.concurrent('expands a glob', (): Promise<void> => {
+    return runList(['gulp*'], {}, 'glob-arg', (config, reporter): ?Promise<void> => {
+      const rprtr = new BufferReporter({});
+      const tree = reporter.getBuffer().slice(-1);
+      const trees = [makeTree('gulp@3.9.1', {color: 'bold'}), makeTree('gulp-babel@6.1.2', {color: 'bold'})];
 
-    rprtr.tree('list', trees);
+      rprtr.tree('list', trees);
 
-    expect(tree).toEqual(rprtr.getBuffer());
+      expect(tree).toEqual(rprtr.getBuffer());
+    });
   });
-});
 
-test('getParent should extract a parent object from a hash, if the parent key exists', () => {
-  const mockTreesByKey = {};
+  test('lists all dependencies when not production', (): Promise<void> => {
+    const nodeEnv = process.env.NODE_ENV;
+    process.env.NODE_ENV = 'development';
+    return runList([], {}, 'dev-deps-prod', (config, reporter): ?Promise<void> => {
+      const rprtr = new BufferReporter({});
+      const tree = reporter.getBuffer().slice(-1);
 
-  mockTreesByKey['parentPkg'] = {
-    name: 'parent@1.1.1',
-    children: [],
-  };
-  const res = getParent('parentPkg#childPkg', mockTreesByKey);
+      // # of devDeps + dependencies should equal 2
+      expect(tree[0].data.trees.length).toEqual(2);
+      expect(reporter.getBuffer()).toMatchSnapshot();
+      process.env.NODE_ENV = nodeEnv;
+    });
+  });
 
-  expect(res instanceof Object).toBe(true);
-  expect(res.name).toBe('parent@1.1.1');
-  expect(res.children.length).toBe(0);
-});
+  test('does not list devDependencies when production', (): Promise<void> => {
+    const nodeEnv = process.env.NODE_ENV;
+    process.env.NODE_ENV = 'production';
+    return runList([], {}, 'dev-deps-prod', (config, reporter): ?Promise<void> => {
+      const rprtr = new BufferReporter({});
+      const tree = reporter.getBuffer().slice(-1);
 
-test('getParent should return undefined if the key does not exist in hash', () => {
-  const mockTreesByKey = {};
-  mockTreesByKey['parentPkg'] = {};
+      // # of dependencies should equal 1
+      expect(tree[0].data.trees.length).toEqual(1);
+      expect(reporter.getBuffer()).toMatchSnapshot();
+      process.env.NODE_ENV = nodeEnv;
+    });
+  });
 
-  const res = getParent('parentPkg#childPkg', mockTreesByKey);
-  expect(res.name).not.toBeDefined();
-  expect(res.children).not.toBeDefined();
-});
+  test('getParent should extract a parent object from a hash, if the parent key exists', () => {
+    const mockTreesByKey = {};
 
-test('getReqDepth should return a number if valid', () => {
-  expect(getReqDepth('1')).toEqual(1);
-  expect(getReqDepth('01')).toEqual(1);
-});
+    mockTreesByKey['parentPkg'] = {
+      name: 'parent@1.1.1',
+      children: [],
+    };
+    const res = getParent('parentPkg#childPkg', mockTreesByKey);
 
-test('getReqDepth should return -1 if invalid', () => {
-  expect(getReqDepth('foo')).toEqual(-1);
-  expect(getReqDepth('bar')).toEqual(-1);
-  expect(getReqDepth('')).toEqual(-1);
+    expect(res instanceof Object).toBe(true);
+    expect(res.name).toBe('parent@1.1.1');
+    expect(res.children.length).toBe(0);
+  });
+
+  test('getParent should return undefined if the key does not exist in hash', () => {
+    const mockTreesByKey = {};
+    mockTreesByKey['parentPkg'] = {};
+
+    const res = getParent('parentPkg#childPkg', mockTreesByKey);
+    expect(res.name).not.toBeDefined();
+    expect(res.children).not.toBeDefined();
+  });
+
+  test('getReqDepth should return a number if valid', () => {
+    expect(getReqDepth('1')).toEqual(1);
+    expect(getReqDepth('01')).toEqual(1);
+  });
+
+  test('getReqDepth should return -1 if invalid', () => {
+    expect(getReqDepth('foo')).toEqual(-1);
+    expect(getReqDepth('bar')).toEqual(-1);
+    expect(getReqDepth('')).toEqual(-1);
+  });
 });
