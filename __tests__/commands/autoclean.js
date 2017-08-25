dfa985b3f59fdbb882582067c475c36873d8de0e
Jeff Valore <rally25rs@yahoo.com>
2017-08-25 11:40:36 -0400

    fix tests. expect statements were not being run.

--- __tests__/commands/autoclean.js
+++ __tests__/commands/autoclean.js
@@ -3,7 +3,7 @@
 import * as fs from '../../src/util/fs.js';
 import {run} from './_helpers.js';
 import {run as autoclean} from '../../src/cli/commands/autoclean.js';
-import {BufferReporter, ConsoleReporter} from '../../src/reporters/index.js';
+import {ConsoleReporter} from '../../src/reporters/index.js';
 import {CLEAN_FILENAME} from '../../src/constants.js';
 import Config from '../../src/config.js';
 import path from 'path';
@@ -13,10 +13,10 @@ const fixturesLoc = path.join(__dirname, '..', 'fixtures', 'autoclean');
 async function runAutoclean(
   flags: Object,
   name: string,
-  checkSteps?: ?(config: Config, output: any) => ?Promise<void>,
+  checkSteps: (config: Config, reporter: ConsoleReporter, output: any) => ?Promise<void>,
 ): Promise<void> {
   await run(
-    BufferReporter,
+    ConsoleReporter,
     fixturesLoc,
     async (args, flags, config, reporter, lockfile): Promise<void> => {
       await autoclean(config, reporter, flags, []);
@@ -24,52 +24,50 @@ async function runAutoclean(
     [],
     flags,
     name,
+    (config, reporter, install, getStdout) => {
+      return checkSteps(config, reporter, getStdout());
+    },
   );
 }
 
 test.concurrent('tells user to run with --init when .yarnclean does not exist', (): Promise<void> => {
-  const reporter = new ConsoleReporter({});
-  return runAutoclean({}, 'not-initialized', (config, output): ?Promise<void> => {
+  return runAutoclean({}, 'not-initialized', (config, reporter, output): ?Promise<void> => {
     expect(output).toContain(reporter.lang('cleanDoesNotExist', CLEAN_FILENAME));
   });
 });
 
 test.concurrent('tells user to run with --init when .yarnclean does not exist and --force', (): Promise<void> => {
-  const reporter = new ConsoleReporter({});
-  return runAutoclean({force: true}, 'not-initialized', (config, output): ?Promise<void> => {
+  return runAutoclean({force: true}, 'not-initialized', (config, reporter, output): ?Promise<void> => {
     expect(output).toContain(reporter.lang('cleanDoesNotExist', CLEAN_FILENAME));
   });
 });
 
 test.concurrent('tells user to edit .yarnclean after init', (): Promise<void> => {
-  const reporter = new ConsoleReporter({});
-  return runAutoclean({init: true}, 'not-initialized', (config, output): ?Promise<void> => {
+  return runAutoclean({init: true}, 'not-initialized', (config, reporter, output): ?Promise<void> => {
     expect(output).toContain(reporter.lang('cleanCreatedFile', CLEAN_FILENAME));
   });
 });
 
 test.concurrent('creates .yarnclean when --init passed', async () => {
-  await runAutoclean({init: true}, 'not-initialized', async (config, output): ?Promise<void> => {
+  await runAutoclean({init: true}, 'not-initialized', async (config, reporter, output): ?Promise<void> => {
     expect(await fs.exists(`${config.cwd}/.yarnclean`)).toEqual(true);
   });
 });
 
 test.concurrent('tells user to run with --force when .yarnclean exists', (): Promise<void> => {
-  const reporter = new ConsoleReporter({});
-  return runAutoclean({}, 'initialized', (config, output): ?Promise<void> => {
+  return runAutoclean({}, 'initialized', (config, reporter, output): ?Promise<void> => {
     expect(output).toContain(reporter.lang('cleanRequiresForce', CLEAN_FILENAME));
   });
 });
 
 test.concurrent('tells user file exists already when --init and .yarnclean exists', (): Promise<void> => {
-  const reporter = new ConsoleReporter({});
-  return runAutoclean({init: true}, 'initialized', (config, output): ?Promise<void> => {
+  return runAutoclean({init: true}, 'initialized', (config, reporter, output): ?Promise<void> => {
     expect(output).toContain(reporter.lang('cleanAlreadyExists', CLEAN_FILENAME));
   });
 });
 
 test.concurrent('runs clean when --force passed and .yarnclean exists', async () => {
-  await runAutoclean({force: true}, 'initialized', async (config, output): ?Promise<void> => {
+  await runAutoclean({force: true}, 'initialized', async (config, reporter, output): ?Promise<void> => {
     expect(await fs.exists(`${config.cwd}/node_modules/left-pad/README.md`)).toEqual(false);
   });
 });
