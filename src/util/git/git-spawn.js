0e492cf63a83d0356c89bc670afd74484e996b17
Burak Yigit Kaya <ben@byk.im>
2017-09-04 18:43:37 +0300

    Fix: respect GIT_SSH env variable when calling Git and support plink
    **Summary**
    
    Fixes #4167. We use `$GIT_SSH_COMMAND` which overrides `$GIT_SSH`
    and causes issues on some systems, where a different `ssh`
    executable like `plink` is used. This patch makes our batch-mode
    enforcement smarter and more respectful and adds support for `plink`.
    
    **Test Plan**
    
    Unit tests.

--- src/util/git/git-spawn.js
+++ src/util/git/git-spawn.js
@@ -1,14 +1,28 @@
 /* @flow */
 
+import path from 'path';
+
 import * as child from '../child.js';
+
+const BATCH_MODE_ARGS = {
+  ssh: '-oBatchMode=yes',
+  plink: '-batch',
+};
 
 // Suppress any password prompts since we run these in the background
 const env = {
   GIT_ASKPASS: '',
   GIT_TERMINAL_PROMPT: 0,
-  GIT_SSH_COMMAND: 'ssh -oBatchMode=yes',
   ...process.env,
 };
+
+const sshCommand = env.GIT_SSH || 'ssh';
+const sshExecutable = path.basename(sshCommand, '.exe');
+const sshBatchArgs = BATCH_MODE_ARGS[sshExecutable];
+
+if (!env.GIT_SSH_COMMAND && sshBatchArgs) {
+  env.GIT_SSH_COMMAND = `${sshCommand} ${sshBatchArgs}`;
+}
 
 export const spawn = (args: Array<string>, opts?: child_process$spawnOpts = {}): Promise<string> => {
   return child.spawn('git', args, {...opts, env});
