dcb953cce6a055694e6e2d10bfbbd81e8e49a2d2
Burak Yigit Kaya <ben@byk.im>
2017-09-06 18:43:19 +0300

    Fix DESTDIR typo

--- src/constants.js
+++ src/constants.js
@@ -82,7 +82,7 @@ function getYarnBinPath(): string {
 export const NODE_MODULES_FOLDER = 'node_modules';
 export const NODE_PACKAGE_JSON = 'package.json';
 
-export const POSIX_GLOBAL_PREFIX = `${process.env.DESTIR || ''}/usr/local`;
+export const POSIX_GLOBAL_PREFIX = `${process.env.DESTDIR || ''}/usr/local`;
 export const FALLBACK_GLOBAL_PREFIX = path.join(userHome, '.yarn');
 
 export const META_FOLDER = '.yarn-meta';
