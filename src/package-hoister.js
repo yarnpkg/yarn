yarnpkg/yarn

commit 9dd9599b9b7e7ddb8b669408aac9f3c0a562d5c7

Fix: Prevent optional subdependencies from being installed with --ignore-optional (#3976)
**Summary**

Fixes #2666.

**Test plan**

Additional unit tests.

src/package-hoister.js
@@ -54,9 +54,11 @@ export class HoistManifest {
 }
 
 export default class PackageHoister {
-  constructor(config: Config, resolver: PackageResolver) {
+  constructor(config: Config, resolver: PackageResolver, {ignoreOptional}: {ignoreOptional: ?boolean} = {}) {
     this.resolver = resolver;
     this.config = config;
+
+    this.ignoreOptional = ignoreOptional;
 
     this.taintedKeys = new Map();
     this.levelQueue = [];
@@ -65,6 +67,8 @@ export default class PackageHoister {
 
   resolver: PackageResolver;
   config: Config;
+
+  ignoreOptional: ?boolean;
 
   levelQueue: Array<[string, HoistManifest]>;
   tree: Map<string, HoistManifest>;
@@ -141,8 +145,11 @@ export default class PackageHoister {
 
     //
     let parentParts: Parts = [];
+
     const isIncompatible = ref.incompatible;
-    let isRequired = isDirectRequire && !ref.ignore && !isIncompatible;
+    const isMarkedAsOptional = ref.optional && this.ignoreOptional;
+
+    let isRequired = isDirectRequire && !ref.ignore && !isIncompatible && !isMarkedAsOptional;
 
     if (parent) {
       if (!this.tree.get(parent.key)) {
@@ -150,7 +157,7 @@ export default class PackageHoister {
       }
       // non ignored dependencies inherit parent's ignored status
       // parent may transition from ignored to non ignored when hoisted if it is used in another non ignored branch
-      if (!isDirectRequire && !isIncompatible && parent.isRequired) {
+      if (!isDirectRequire && !isIncompatible && parent.isRequired && !isMarkedAsOptional) {
         isRequired = true;
       }
       parentParts = parent.parts;
@@ -196,7 +203,13 @@ export default class PackageHoister {
 
       for (const depPattern of ref.dependencies) {
         const depinfo = this._lookupDependency(info, depPattern);
-        if (depinfo && !depinfo.isRequired && !depinfo.isIncompatible) {
+
+        if (!depinfo) {
+          continue;
+        }
+
+        const isMarkedAsOptional = !depinfo.pkg._reference || this.ignoreOptional;
+        if (!depinfo.isRequired && !depinfo.isIncompatible && !isMarkedAsOptional) {
           depinfo.isRequired = true;
           depinfo.addHistory(`Mark as non-ignored because of usage by ${info.key}`);
           toVisit.push(depinfo);
@@ -544,7 +557,6 @@ export default class PackageHoister {
     for (const [loc, info] of flatTree) {
       const ref = info.pkg._reference;
       invariant(ref, 'expected reference');
-
       if (!info.isRequired) {
         info.addHistory('Deleted as this module was ignored');
       } else {

__tests__/commands/install/integration.js
@@ -48,6 +48,18 @@ async function mockConstants(base: Config, mocks: Object, cb: (config: Config) =
 beforeEach(request.__resetAuthedRequests);
 afterEach(request.__resetAuthedRequests);
 
+test.concurrent('install optional subdependencies by default', async () => {
+  await runInstall({}, 'install-optional-dependencies', async (config): Promise<void> => {
+    expect(await fs.exists(`${config.cwd}/node_modules/dep-b`)).toEqual(true);
+  });
+});
+
+test.concurrent('installing with --ignore-optional should not install optional subdependencies', async () => {
+  await runInstall({ignoreOptional: true}, 'install-optional-dependencies', async (config): Promise<void> => {
+    expect(await fs.exists(`${config.cwd}/node_modules/dep-b`)).toEqual(false);
+  });
+});
+
 test.concurrent('packages installed through the link protocol should validate all peer dependencies', async () => {
   await runInstall({checkFiles: true}, 'check-files-should-not-cross-symlinks', async (config): Promise<void> => {
     expect(

src/cli/commands/install.js
@@ -278,12 +278,7 @@ export class Install {
 
       pushDeps('dependencies', projectManifestJson, {hint: null, optional: false}, true);
       pushDeps('devDependencies', projectManifestJson, {hint: 'dev', optional: false}, !this.config.production);
-      pushDeps(
-        'optionalDependencies',
-        projectManifestJson,
-        {hint: 'optional', optional: true},
-        !this.flags.ignoreOptional,
-      );
+      pushDeps('optionalDependencies', projectManifestJson, {hint: 'optional', optional: true}, true);
 
       if (this.config.workspacesEnabled) {
         const workspaces = await this.config.resolveWorkspaces(path.dirname(loc), projectManifestJson);
@@ -461,7 +456,10 @@ export class Install {
       // remove integrity hash to make this operation atomic
       await this.integrityChecker.removeIntegrityFile();
       this.reporter.step(curr, total, this.reporter.lang('linkingDependencies'), emoji.get('link'));
-      await this.linker.init(flattenedTopLevelPatterns, this.flags.linkDuplicates, workspaceLayout);
+      await this.linker.init(flattenedTopLevelPatterns, workspaceLayout, {
+        linkDuplicates: this.flags.linkDuplicates,
+        ignoreOptional: this.flags.ignoreOptional,
+      });
     });
 
     steps.push(async (curr: number, total: number) => {

src/package-linker.js
@@ -119,18 +119,21 @@ export default class PackageLinker {
     }
   }
 
-  getFlatHoistedTree(patterns: Array<string>): Promise<HoistManifestTuples> {
-    const hoister = new PackageHoister(this.config, this.resolver);
+  getFlatHoistedTree(
+    patterns: Array<string>,
+    {ignoreOptional}: {ignoreOptional: ?boolean} = {},
+  ): Promise<HoistManifestTuples> {
+    const hoister = new PackageHoister(this.config, this.resolver, {ignoreOptional});
     hoister.seed(patterns);
     return Promise.resolve(hoister.init());
   }
 
   async copyModules(
     patterns: Array<string>,
-    linkDuplicates: boolean,
     workspaceLayout?: WorkspaceLayout,
+    {linkDuplicates, ignoreOptional}: {linkDuplicates: ?boolean, ignoreOptional: ?boolean} = {},
   ): Promise<void> {
-    let flatTree = await this.getFlatHoistedTree(patterns);
+    let flatTree = await this.getFlatHoistedTree(patterns, {ignoreOptional});
 
     // sorted tree makes file creation and copying not to interfere with each other
     flatTree = flatTree.sort(function(dep1, dep2): number {
@@ -429,8 +432,12 @@ export default class PackageLinker {
     return range === '*' || satisfiesWithPreleases(version, range, this.config.looseSemver);
   }
 
-  async init(patterns: Array<string>, linkDuplicates: boolean, workspaceLayout?: WorkspaceLayout): Promise<void> {
+  async init(
+    patterns: Array<string>,
+    workspaceLayout?: WorkspaceLayout,
+    {linkDuplicates, ignoreOptional}: {linkDuplicates: ?boolean, ignoreOptional: ?boolean} = {},
+  ): Promise<void> {
     this.resolvePeerModules();
-    await this.copyModules(patterns, linkDuplicates, workspaceLayout);
+    await this.copyModules(patterns, workspaceLayout, {linkDuplicates, ignoreOptional});
   }
 }

commit 445003fcf9f231c96cd3a9e8cc37304ba3e938b7

Fix: incorrect optional ignore in nested dependencies
**Summary**

Fixes #4445. The bug was introduced in
https://github.com/yarnpkg/yarn/pull/3976/files#diff-7066979f95168f7bb59e4f9cfb3ba8fcR211
with an incomplete package optional check, causing many transient
dependencies to be ignored at linking stage when `--ignore-optional`
flag is passed. This patch fixes the check to combine the flag with
the package's actual optional mark.

**Test plan**

Manually verified #4445 is resolved. Also added a unit test.

src/package-hoister.js
@@ -242,7 +242,7 @@ export default class PackageHoister {
           continue;
         }
 
-        const isMarkedAsOptional = !depinfo.pkg._reference || this.ignoreOptional;
+        const isMarkedAsOptional = !depinfo.pkg._reference || (depinfo.pkg._reference.optional && this.ignoreOptional);
         if (!depinfo.isRequired && !depinfo.isIncompatible && !isMarkedAsOptional) {
           depinfo.isRequired = true;
           depinfo.addHistory(`Mark as non-ignored because of usage by ${info.key}`);