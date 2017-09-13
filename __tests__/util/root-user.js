78aefd440efe7c973a5e9282f40b081b2c2185b3
Martin Stuecklschwaiger <martin.stuecklschwaiger@walls.io>
2017-09-13 17:51:26 +0200

    Add test for isFakeRoot

--- __tests__/util/root-user.js
+++ __tests__/util/root-user.js
@@ -1,9 +1,20 @@
 /* @flow */
 
-import {isRootUser} from '../../src/util/root-user.js';
+import {isRootUser, isFakeRoot} from '../../src/util/root-user.js';
 
 test('isRootUser', () => {
   expect(isRootUser(null)).toBe(false);
   expect(isRootUser(1001)).toBe(false);
   expect(isRootUser(0)).toBe(true);
+});
+
+test('isFakeRoot', () => {
+  delete process.env.FAKEROOTKEY;
+
+  expect(isFakeRoot()).toBe(false);
+
+  process.env.FAKEROOTKEY = '15574641';
+  expect(isFakeRoot()).toBe(true);
+
+  delete process.env.FAKEROOTKEY;
 });
