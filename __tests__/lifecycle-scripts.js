3124c91c99bb0ada6c242133d0da35ee7a2740fd
Aleksei Gusev <aleksei.gusev@gmail.com>
2017-09-18 18:55:27 +0300

    Chore: Free tests from FORCE_COLOR environment variable dependency (#4381)
    **Summary**
    
    FORCE_COLOR environment variable allows to force yarn to use
    colors. This makes some of tests fail because of comparing
    colorized output with plain strings.
    
    For example, if you run a test in environment where
    FORCE_COLOR is set you get this failure:
    
    ```
     FAIL  __tests__\index.js
      ● should add package
        expect(received).toEqual(expected)
        Expected value to equal:
          "success Saved lockfile."
        Received:
    success Saved lockfile."
    ```
    
    **Test plan**
    
    Run tests in an environment where `FORCE_COLOR` is enabled. They should pass.

--- __tests__/lifecycle-scripts.js
+++ __tests__/lifecycle-scripts.js
@@ -19,14 +19,15 @@ async function execCommand(cmd: string, packageName: string, env = process.env):
   await fs.copy(srcPackageDir, packageDir, new NoopReporter());
 
   return new Promise((resolve, reject) => {
+    const cleanedEnv = {...env};
+    cleanedEnv['YARN_SILENT'] = 0;
+    delete cleanedEnv['FORCE_COLOR'];
+
     exec(
       `node "${yarnBin}" ${cmd}`,
       {
         cwd: packageDir,
-        env: {
-          ...env,
-          YARN_SILENT: 0,
-        },
+        env: cleanedEnv,
       },
       (err, stdout) => {
         if (err) {
