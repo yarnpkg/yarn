bcf78f3dee601bae655b75e8ba9ffb0b65f5545e
Yüksel Ergüden <yerguden@gmail.com>
2017-09-27 00:49:57 +0300

    Fix: prevent init command from traversing up. (#4441)
    **Summary**
    
    Fixes #4441.
    
    **Test plan**
    Manual verification / unit tests / integration tests.

--- src/cli/index.js
+++ src/cli/index.js
@@ -454,7 +454,7 @@ export function main({
     return errorReportLoc;
   }
 
-  const cwd = findProjectRoot(commander.cwd);
+  const cwd = command.shouldRunInCurrentCwd ? commander.cwd : findProjectRoot(commander.cwd);
 
   config
     .init({
