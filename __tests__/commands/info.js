c3b0d65d0f5436aa4a30ee5722ca38732aa02b3a
Krzysztof Zbudniewek <krzysztof.zbudniewek@gmail.com>
2017-10-29 02:29:53 +0200

    Fix linter issues by shortening the test description

--- __tests__/commands/info.js
+++ __tests__/commands/info.js
@@ -88,7 +88,7 @@ test.concurrent('with two arguments and second argument "readme" shows readme st
   });
 });
 
-test.concurrent('with two arguments and second argument "version" shows version with `latest` tag', (): Promise<void> => {
+test.concurrent('with two arguments and second argument "version" shows `latest` version', (): Promise<void> => {
   return runInfo(['ui-select', 'version'], {}, '', (config, output): ?Promise<void> => {
     expect(output).toEqual('0.19.8');
   });
