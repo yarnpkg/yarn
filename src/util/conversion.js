f5c29421b36e8669de5926edaa9d3788556e46f9
Stephan Hohe <sth.dev@tejp.de>
2017-11-04 02:58:42 +0100

    Syntax prettification

--- src/util/conversion.js
+++ src/util/conversion.js
@@ -1,11 +1,11 @@
 /* @flow */
 
-export function boolify(val: string|number|boolean): boolean {
+export function boolify(val: string | number | boolean): boolean {
   const strVal = val.toString().toLowerCase();
   return strVal !== 'false' && strVal !== '0';
 }
 
-export function boolifyWithDefault(val: ?(string|number|boolean), defaultResult: boolean): boolean {
+export function boolifyWithDefault(val: ?(string | number | boolean), defaultResult: boolean): boolean {
   if (val === undefined || val === null || val === '') {
     return defaultResult;
   } else {
