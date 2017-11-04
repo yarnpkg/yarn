9d72d18a776f342995b89b0daacb56e5e351d397
Stephan Hohe <sth.dev@tejp.de>
2017-11-04 02:53:11 +0100

    Stricter types

--- src/util/conversion.js
+++ src/util/conversion.js
@@ -1,10 +1,10 @@
 /* @flow */
 
-export function boolify(val: any): boolean {
+export function boolify(val: string|number|boolean): boolean {
   return val.toString().toLowerCase() !== 'false' && val !== '0';
 }
 
-export function boolifyWithDefault(val: any, defaultResult: boolean): boolean {
+export function boolifyWithDefault(val: ?(string|number|boolean), defaultResult: boolean): boolean {
   if (val === undefined || val === null || val === '') {
     return defaultResult;
   } else {
