bff9972cc7022a7d3213b0f70d5fe1044758e65c
Nicholas Westbury <5westbury5@gmail.com>
2017-11-05 20:45:44 -0500

    fixed newlines for windows build

--- __tests__/integration.js
+++ __tests__/integration.js
@@ -417,10 +417,11 @@ test('yarn install --offline with git hash', async () => {
   const cwd = await makeTemp();
   const cacheFolder = path.join(cwd, 'cache');
   const nodeModules = path.join(cwd, 'node_modules');
+  const nl = process.platform === 'win32' ? '\r\n' : '\n';
 
   await fs.writeFile(
     path.join(cwd, '.yarnrc'),
-    `yarn-offline-mirror "${cacheFolder}"\nyarn-offline-mirror-pruning true`,
+    `yarn-offline-mirror "${cacheFolder}"${nl}yarn-offline-mirror-pruning true${nl}`,
   );
 
   await fs.writeFile(
