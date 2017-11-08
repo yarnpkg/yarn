df322bff0b2be82487a186ae5f36c7cd646df737
Maël Nison <mael@fb.com>
2017-11-08 14:18:20 +0000

    Adds a "workspaces" command that print info about the workspaces layout

---
+++ __tests__/commands/workspaces.js
@@ -0,0 +1,40 @@
+// @flow
+
+import {BufferReporter} from '../../src/reporters/index.js';
+import {run as workspace} from '../../src/cli/commands/workspaces.js';
+import * as reporters from '../../src/reporters/index.js';
+import Config from '../../src/config.js';
+import path from 'path';
+
+const fixturesLoc = path.join(__dirname, '..', 'fixtures', 'workspace');
+
+async function runWorkspaces(
+  flags: Object,
+  args: Array<string>,
+  name: string,
+  checkSteps?: ?(config: Config, reporter: BufferReporter) => ?Promise<void>,
+): Promise<void> {
+  const cwd = path.join(fixturesLoc, name);
+  const reporter = new reporters.BufferReporter({stdout: null, stdin: null, isSilent: true});
+
+  const config = await Config.create({cwd}, reporter);
+
+  await workspace(config, reporter, flags, args);
+
+  if (checkSteps) {
+    await checkSteps(config, reporter);
+  }
+}
+
+test('workspaces should list the workspaces', (): Promise<void> => {
+  return runWorkspaces({}, ['info'], 'run-basic', (config, reporter) => {
+    expect(reporter.getBufferJson()).toEqual({
+      'workspace-1': {
+        location: 'packages/workspace-child-1',
+      },
+      'workspace-2': {
+        location: 'packages/workspace-child-2',
+      },
+    });
+  });
+});
---
+++ src/cli/commands/workspaces.js
@@ -0,0 +1,44 @@
+// @flow
+
+import type Config from '../../config.js';
+import {MessageError} from '../../errors.js';
+import type {Reporter} from '../../reporters/index.js';
+import buildSubCommands from './_build-sub-commands.js';
+
+const invariant = require('invariant');
+const path = require('path');
+
+export function hasWrapper(commander: Object, args: Array<string>): boolean {
+  return true;
+}
+
+export async function info(config: Config, reporter: Reporter, flags: Object, args: Array<string>): Promise<void> {
+  const {workspaceRootFolder} = config;
+
+  if (!workspaceRootFolder) {
+    throw new MessageError(reporter.lang('workspaceRootNotFound', config.cwd));
+  }
+
+  const manifest = await config.findManifest(workspaceRootFolder, false);
+  invariant(manifest && manifest.workspaces, 'We must find a manifest with a "workspaces" property');
+
+  const workspaces = await config.resolveWorkspaces(workspaceRootFolder, manifest);
+
+  const publicData = {};
+
+  for (const workspaceName of Object.keys(workspaces)) {
+    publicData[workspaceName] = {
+      location: path.relative(config.lockfileFolder, workspaces[workspaceName].loc),
+    };
+  }
+
+  reporter.log(JSON.stringify(publicData, null, 2), {force: true});
+}
+
+const {run, setFlags, examples} = buildSubCommands('cache', {
+  async info(config: Config, reporter: Reporter, flags: Object, args: Array<string>): Promise<void> {
+    await info(config, reporter, flags, args);
+  },
+});
+
+export {run, setFlags, examples};
