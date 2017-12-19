bad52373504cdebb9a60f96e5b34f008864cbcc7
Ian Sutherland <ian@iansutherland.ca>
2017-12-19 10:58:21 -0700

    feat(cli): group by license in licenses list (#5074)

--- src/cli/commands/licenses.js
+++ src/cli/commands/licenses.js
@@ -49,44 +49,71 @@ async function getManifests(config: Config, flags: Object): Promise<Array<Manife
 
 async function list(config: Config, reporter: Reporter, flags: Object, args: Array<string>): Promise<void> {
   const manifests: Array<Manifest> = await getManifests(config, flags);
+  const manifestsByLicense = new Map();
+
+  for (const {name, version, license, repository, homepage, author} of manifests) {
+    const licenseKey = license || 'UNKNOWN';
+    const url = repository ? repository.url : homepage;
+    const vendorUrl = homepage || (author && author.url);
+    const vendorName = author && author.name;
+
+    if (!manifestsByLicense.has(licenseKey)) {
+      manifestsByLicense.set(licenseKey, new Map());
+    }
+
+    const byLicense = manifestsByLicense.get(licenseKey);
+    invariant(byLicense, 'expected value');
+    byLicense.set(`${name}@${version}`, {
+      name,
+      version,
+      url,
+      vendorUrl,
+      vendorName,
+    });
+  }
 
   if (flags.json) {
     const body = [];
 
-    for (const {name, version, license, repository, homepage, author} of manifests) {
-      const url = repository ? repository.url : homepage;
-      const vendorUrl = homepage || (author && author.url);
-      const vendorName = author && author.name;
-      body.push([
-        name,
-        version,
-        license || 'Unknown',
-        url || 'Unknown',
-        vendorUrl || 'Unknown',
-        vendorName || 'Unknown',
-      ]);
-    }
+    manifestsByLicense.forEach((license, licenseKey) => {
+      license.forEach(({name, version, url, vendorUrl, vendorName}) => {
+        body.push([name, version, licenseKey, url || 'Unknown', vendorUrl || 'Unknown', vendorName || 'Unknown']);
+      });
+    });
 
     reporter.table(['Name', 'Version', 'License', 'URL', 'VendorUrl', 'VendorName'], body);
   } else {
     const trees = [];
 
-    for (const {name, version, license, repository, homepage} of manifests) {
-      const children = [];
-      children.push({
-        name: `${reporter.format.bold('License:')} ${license || reporter.format.red('UNKNOWN')}`,
-      });
+    manifestsByLicense.forEach((license, licenseKey) => {
+      const licenseTree = [];
 
-      const url = repository ? repository.url : homepage;
-      if (url) {
-        children.push({name: `${reporter.format.bold('URL:')} ${url}`});
-      }
+      license.forEach(({name, version, url, vendorUrl, vendorName}) => {
+        const children = [];
+
+        if (url) {
+          children.push({name: `${reporter.format.bold('URL:')} ${url}`});
+        }
+
+        if (vendorUrl) {
+          children.push({name: `${reporter.format.bold('VendorUrl:')} ${vendorUrl}`});
+        }
+
+        if (vendorName) {
+          children.push({name: `${reporter.format.bold('VendorName:')} ${vendorName}`});
+        }
+
+        licenseTree.push({
+          name: `${name}@${version}`,
+          children,
+        });
+      });
 
       trees.push({
-        name: `${name}@${version}`,
-        children,
+        name: licenseKey,
+        children: licenseTree,
       });
-    }
+    });
 
     reporter.tree('licenses', trees);
   }
