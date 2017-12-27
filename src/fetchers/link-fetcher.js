7a92d10ea949056e97f70ea767e3022c26ec4dec
Jeff Valore <rally25rs@yahoo.com>
2017-12-26 21:15:59 -0500

    Refactor LinkFetcher to extend BaseFetcher

--- src/fetchers/link-fetcher.js
+++ src/fetchers/link-fetcher.js
@@ -1,28 +1,11 @@
 /* @flow */
 
-import type {PackageRemote, FetchedMetadata, Manifest} from '../types.js';
-import type Config from '../config.js';
-import type {RegistryNames} from '../registries/index.js';
+import type {FetchedMetadata, Manifest} from '../types.js';
+import BaseFetcher from './base-fetcher.js';
 import * as fs from '../util/fs.js';
 
-export default class LinkFetcher {
-  constructor(dest: string, remote: PackageRemote, config: Config) {
-    this.config = config;
-    this.dest = dest;
-    this.reference = remote.reference;
-    this.registry = remote.registry;
-  }
-
-  config: Config;
-  dest: string;
-  registry: RegistryNames;
-  reference: string;
-
-  setupMirrorFromCache(): Promise<?string> {
-    return Promise.resolve();
-  }
-
-  async fetch(): Promise<FetchedMetadata> {
+export default class LinkFetcher extends BaseFetcher {
+  async fetch(defaultManifest: ?Object): Promise<FetchedMetadata> {
     const sourceExists = await fs.exists(this.reference);
     let pkg: Manifest = {_uid: '', name: '', version: '0.0.0'};
 
