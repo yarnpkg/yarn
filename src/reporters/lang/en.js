eaa9be87e0a086603dc598bf9fa86480a9d1690a
Ian Sutherland <ian@iansutherland.ca>
2018-01-15 06:31:14 -0700

    feat(cli): improve error message when file resolver can't find file (#5134) (#5145)
    * feat(cli): improve error message when file resolver can't find file (#5134)
    
    * Update en.js
    
    * Update en.js

--- src/reporters/lang/en.js
+++ src/reporters/lang/en.js
@@ -80,7 +80,7 @@ const messages = {
   invalidVersion: 'Invalid version supplied.',
   requiredVersionInRange: 'Required version in range.',
   packageNotFoundRegistry: "Couldn't find package $0 on the $1 registry.",
-  doesntExist: "$0 doesn't exist.",
+  doesntExist: "Package $1 refers to a non-existing file '$0'.",
   missingRequiredPackageKey: `Package $0 doesn't have a $1.`,
   invalidAccess: 'Invalid argument for access, expected public or restricted.',
   invalidCommand: 'Invalid subcommand. Try $0',
