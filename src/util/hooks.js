4386744b3ac5e5fc195fc7ed1647033ced96ea59
Maël Nison <mael@fb.com>
2018-01-30 12:38:49 +0000

    feat(install): Implement a very basic hook system
    This PR adds a very basic and undocumented hook system. I plan to use it internally to get better
    stats on how Yarn performs, and how much time is spent on the linking step.

---
+++ src/util/hooks.js
@@ -0,0 +1,21 @@
+/* @flow */
+
+const YARN_HOOKS_KEY = 'experimentalYarnHooks';
+
+export function callThroughHook<T>(type: string, fn: () => T): T {
+  if (typeof global === 'undefined') {
+    return fn();
+  }
+
+  if (typeof global[YARN_HOOKS_KEY] !== 'object' || !global[YARN_HOOKS_KEY]) {
+    return fn();
+  }
+
+  const hook: (() => T) => T = global[YARN_HOOKS_KEY][type];
+
+  if (!hook) {
+    return fn();
+  }
+
+  return hook(fn);
+}
