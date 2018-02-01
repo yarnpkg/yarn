6ef04eb1ead9f4e09d0394defe56f71660fa42dd
Maël Nison <mael@fb.com>
2018-02-01 11:06:02 +0000

    Adds tests

---
+++ __tests__/hooks.js
@@ -0,0 +1,42 @@
+import * as fs from '../../../src/util/fs.js';
+import {runInstall} from './commands/_helpers.js';
+
+test('install should call the resolveStep hook', async () => {
+  global.experimentalYarnHooks = {
+    resolveStep: jest.fn(cb => cb()),
+  };
+  await runInstall({}, 'install-production', async (config): Promise<void> => {
+    expect(global.experimentalYarnHooks.resolveStep.mock.calls.length).toEqual(1);
+  });
+  delete global.experimentalYarnHooks;
+});
+
+test('install should call the fetchStep hook', async () => {
+  global.experimentalYarnHooks = {
+    fetchStep: jest.fn(cb => cb()),
+  };
+  await runInstall({}, 'install-production', async (config): Promise<void> => {
+    expect(global.experimentalYarnHooks.fetchStep.mock.calls.length).toEqual(1);
+  });
+  delete global.experimentalYarnHooks;
+});
+
+test('install should call the linkStep hook', async () => {
+  global.experimentalYarnHooks = {
+    linkStep: jest.fn(cb => cb()),
+  };
+  await runInstall({}, 'install-production', async (config): Promise<void> => {
+    expect(global.experimentalYarnHooks.linkStep.mock.calls.length).toEqual(1);
+  });
+  delete global.experimentalYarnHooks;
+});
+
+test('install should call the buildStep hook', async () => {
+  global.experimentalYarnHooks = {
+    buildStep: jest.fn(cb => cb()),
+  };
+  await runInstall({}, 'install-production', async (config): Promise<void> => {
+    expect(global.experimentalYarnHooks.buildStep.mock.calls.length).toEqual(1);
+  });
+  delete global.experimentalYarnHooks;
+});
