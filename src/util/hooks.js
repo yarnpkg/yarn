aee005aa16a929a69730235e4f178f5a7bc5f81b
Maël Nison <nison.mael@gmail.com>
2018-02-02 18:39:25 +0000

    feat(install): Implement a very basic hook system (#5293)
    * feat(install): Implement a very basic hook system
    
    This PR adds a very basic and undocumented hook system. I plan to use it internally to get better
    stats on how Yarn performs, and how much time is spent on the linking step.
    
    * Adds tests
    
    * Improves the typing of callThroughHook
    
    * Lints

---
+++ src/util/hooks.js
@@ -0,0 +1,23 @@
+/* @flow */
+
+export type YarnHook = 'resolveStep' | 'fetchStep' | 'linkStep' | 'buildStep';
+
+const YARN_HOOKS_KEY = 'experimentalYarnHooks';
+
+export function callThroughHook<T>(type: YarnHook, fn: () => T): T {
+  if (typeof global === 'undefined') {
+    return fn();
+  }
+
+  if (typeof global[YARN_HOOKS_KEY] !== 'object' || !global[YARN_HOOKS_KEY]) {
+    return fn();
+  }
+
+  const hook: (() => T) => T = global[YARN_HOOKS_KEY][type];
+
+  if (!hook) {
+    return fn();
+  }
+
+  return hook(fn);
+}
