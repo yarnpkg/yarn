437dda2e41319caf33b09277f965edf5f94710b6
Mike Chen <mhchen@gmail.com>
2018-02-04 15:35:36 -0800

    fix(cli): fail when `yarn add`ing Github package with no version (#5292)

--- src/reporters/lang/en.js
+++ src/reporters/lang/en.js
@@ -89,6 +89,7 @@ const messages = {
   invalidHostedGitFragment: 'Invalid hosted git fragment $0.',
   invalidFragment: 'Invalid fragment $0.',
   invalidPackageName: 'Invalid package name.',
+  invalidPackageVersion: "Can't add $0: invalid package version $1.",
   couldntFindManifestIn: "Couldn't find manifest in $0.",
   shrinkwrapWarning:
     'npm-shrinkwrap.json found. This will not be updated or respected. See https://yarnpkg.com/en/docs/migrating-from-npm for more information.',
