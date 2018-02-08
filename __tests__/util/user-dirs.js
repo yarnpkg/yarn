4368d1f2d51f53689bfd3c5fc9bc8db1b6dfed1d
Will Binns-Smith <wbinnssmith@fb.com>
2018-02-08 15:40:54 -0800

    Fall back to ~/.config/yarn instead of XDG/Windows paths

--- __tests__/util/user-dirs.js
+++ __tests__/util/user-dirs.js
@@ -17,8 +17,8 @@ describe('getDataDir', () => {
       delete process.env.LOCALAPPDATA;
     });
 
-    test('uses AppData\\Local\\Data otherwise', () => {
-      expect(getDataDir()).toBe(path.join(userHome, 'AppData', 'Local', 'Yarn', 'Data'));
+    test('uses the config dir otherwise', () => {
+      expect(getDataDir()).toBe(path.join(userHome, '.config', 'yarn'));
     });
   });
 
@@ -33,8 +33,8 @@ describe('getDataDir', () => {
       delete process.env.XDG_DATA_HOME;
     });
 
-    test('falls back to .local/share/yarn', () => {
-      expect(getDataDir()).toBe(path.join(userHome, '.local', 'share', 'yarn'));
+    test('falls back to the config dir', () => {
+      expect(getDataDir()).toBe(path.join(userHome, '.config', 'yarn'));
     });
   });
 });
@@ -101,8 +101,8 @@ describe('getConfigDir', () => {
       delete process.env.LOCALAPPDATA;
     });
 
-    test('uses AppData\\Local\\Config otherwise', () => {
-      expect(getConfigDir()).toBe(path.join(userHome, 'AppData', 'Local', 'Yarn', 'Config'));
+    test('uses the config dir otherwise', () => {
+      expect(getConfigDir()).toBe(path.join(userHome, '.config', 'yarn'));
     });
   });
 
