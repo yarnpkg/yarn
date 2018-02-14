9e756528c80eb1cbfeac542ada6a242416591444
Pig Fang <g-plane@hotmail.com>
2018-02-14 16:36:13 +0800

    feat(remove): add emoji for steps

--- src/cli/commands/remove.js
+++ src/cli/commands/remove.js
@@ -11,6 +11,7 @@ import * as fs from '../../util/fs.js';
 import * as constants from '../../constants.js';
 
 const path = require('path');
+const emoji = require('node-emoji');
 
 export const requireLockfile = true;
 
@@ -36,7 +37,7 @@ export async function run(config: Config, reporter: Reporter, flags: Object, arg
   const manifests = [];
 
   for (const name of args) {
-    reporter.step(++step, totalSteps, `Removing module ${name}`);
+    reporter.step(++step, totalSteps, `Removing module ${name}`, emoji.get('wastebasket'));
 
     let found = false;
 
@@ -77,7 +78,7 @@ export async function run(config: Config, reporter: Reporter, flags: Object, arg
   }
 
   // reinstall so we can get the updated lockfile
-  reporter.step(++step, totalSteps, reporter.lang('uninstallRegenerate'));
+  reporter.step(++step, totalSteps, reporter.lang('uninstallRegenerate'), emoji.get('page_with_curl'));
   const installFlags = {force: true, workspaceRootIsCwd: true, ...flags};
   const reinstall = new Install(installFlags, config, new NoopReporter(), lockfile);
   await reinstall.init();
