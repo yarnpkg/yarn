831493b6f454eecfabe71173beafee9caf6ae6ff
Maël Nison <nison.mael@gmail.com>
2018-02-23 13:11:57 +0000

    Update workspaces.js

--- src/cli/commands/workspaces.js
+++ src/cli/commands/workspaces.js
@@ -39,8 +39,8 @@ export async function info(config: Config, reporter: Reporter, flags: Object, ar
         for (const dependencyName of Object.keys(manifest[dependencyType] || {})) {
           if (Object.prototype.hasOwnProperty.call(workspaces, dependencyName)) {
             invariant(manifest && manifest[dependencyType], 'The request should exist');
-            const request = manifest[dependencyType][dependencyName];
-            if (semver.satisfies(workspaces[dependencyName].manifest.version, request)) {
+            const requestedRange = manifest[dependencyType][dependencyName];
+            if (semver.satisfies(workspaces[dependencyName].manifest.version, requestedRange)) {
               workspaceDependencies.add(dependencyName);
             } else {
               mismatchedWorkspaceDependencies.add(dependencyName);
