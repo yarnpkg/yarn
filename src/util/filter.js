953c3034e0bd4fb8510e5e8da1393ac4746d8c21
Christopher Mühl <christopher@dieschittigs.de>
2018-02-27 08:53:07 +0100

    Only filter by basename if base is not set

--- src/util/filter.js
+++ src/util/filter.js
@@ -95,13 +95,20 @@ export function sortFilter(
 }
 
 export function matchesFilter(filter: IgnoreFilter, basename: string, loc: string): boolean {
+  let filterByBasename = true;
   if (filter.base && filter.base !== '.') {
     loc = path.relative(filter.base, loc);
+    filterByBasename = false;
   }
   // the micromatch regex expects unix path separators
   loc = loc.replace('\\', '/');
 
-  return filter.regex.test(loc) || filter.regex.test(`/${loc}`) || mm.isMatch(loc, filter.pattern);
+  return (
+    filter.regex.test(loc) ||
+    filter.regex.test(`/${loc}`) ||
+    (filterByBasename && filter.regex.test(basename)) ||
+    mm.isMatch(loc, filter.pattern)
+  );
 }
 
 export function ignoreLinesToRegex(lines: Array<string>, base: string = '.'): Array<IgnoreFilter> {
