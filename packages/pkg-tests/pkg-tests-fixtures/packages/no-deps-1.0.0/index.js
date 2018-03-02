cf00d52e0aea194b6feb69d0fc1fafbfe03f93a3
Maël Nison <mael@fb.com>
2018-03-02 19:06:52 +0000

    Improves the fixtures so that they also return info about their dev and peer dependencies
    Branch: dev-peer-fixtures

--- packages/pkg-tests/pkg-tests-fixtures/packages/no-deps-1.0.0/index.js
+++ packages/pkg-tests/pkg-tests-fixtures/packages/no-deps-1.0.0/index.js
@@ -1,5 +1,10 @@
+/* @flow */
+
 module.exports = require(`./package.json`);
 
-for (const key of Object.keys(module.exports.dependencies || {})) {
-  module.exports.dependencies[key] = require(key);
+for (const key of [`dependencies`, `devDependencies`, `peerDependencies`]) {
+  for (const dep of Object.keys(module.exports[key] || {})) {
+    // $FlowFixMe The whole point of this file is to be dynamic
+    module.exports[key][dep] = require(dep);
+  }
 }
