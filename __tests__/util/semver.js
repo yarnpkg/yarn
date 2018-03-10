2e79cf399b82f8aee73ec78a466b445c1bc866bd
Simon Kjellberg <simon.kjellberg@gmail.com>
2018-03-10 18:00:39 +0100

    fix(cli): adjust colors for unstable version updates
    This change special-cases the color indicator for updates to
    *unstable* package versions, to more accurately indicate
    backwards compatibility.
    
    For 0.x.y versions, increments to the *minor* version number will
    be considered a *major* version change, and increments to the
    *patch* number, will be considered a *minor* version change.
    
    For 0.0.x versions, increments to *any* version number will be
    considered a *major* version change.
    
    This matches the behavior of how yarn & npm treats updates to
    packages defined with a caret-range.

--- __tests__/util/semver.js
+++ __tests__/util/semver.js
@@ -1,8 +1,7 @@
 /* @flow */
 
-import {satisfiesWithPrereleases} from '../../src/util/semver.js';
-
-const semver = require('semver');
+import semver from 'semver';
+import {satisfiesWithPrereleases, diffWithUnstable} from '../../src/util/semver.js';
 
 describe('satisfiesWithPrereleases', () => {
   it('matches the behavior of node-semver for non-prerelease versions', () => {
@@ -52,5 +51,46 @@ describe('satisfiesWithPrereleases', () => {
     expect(satisfiesWithPrereleases('1.0.0-beta.11', '>1.0.0-beta.2')).toBe(true);
     expect(satisfiesWithPrereleases('1.0.0-rc.1', '>1.0.0-beta.11')).toBe(true);
     expect(satisfiesWithPrereleases('1.0.0', '>1.0.0-rc.1')).toBe(true);
+  });
+});
+
+describe('diffWithUnstable', () => {
+  it('matches the behavior of node-semver for stable versions', () => {
+    expect(diffWithUnstable('2.0.0', '1.0.0')).toBe(semver.diff('2.0.0', '1.0.0'));
+    expect(diffWithUnstable('2.1.0', '2.0.0')).toBe(semver.diff('2.1.0', '2.0.0'));
+    expect(diffWithUnstable('2.1.1', '2.1.0')).toBe(semver.diff('2.1.1', '2.1.0'));
+    expect(diffWithUnstable('2.1.1-beta', '2.1.0')).toBe(semver.diff('2.1.1-beta', '2.1.0'));
+    expect(diffWithUnstable('2.1.1-alpha', '2.1.1')).toBe(semver.diff('2.1.1-alpha', '2.1.1'));
+    expect(diffWithUnstable('2.1.1', '2.1.1')).toBe(semver.diff('2.1.1', '2.1.1'));
+    expect(diffWithUnstable('2.1.1-rc', '2.1.1-rc')).toBe(semver.diff('2.1.1-rc', '2.1.1-rc'));
+  });
+
+  it('treats patch and minor releases for pre-major versions as minor & major respectively', () => {
+    expect(diffWithUnstable('0.2.0', '0.2.1')).toBe('minor');
+    expect(diffWithUnstable('0.2.0', '0.2.1-rc')).toBe('preminor');
+    expect(diffWithUnstable('0.1.0', '0.2.0')).toBe('major');
+    expect(diffWithUnstable('0.1.0', '0.2.0-alpha')).toBe('premajor');
+    expect(diffWithUnstable('0.2.1', '1.0.0')).toBe('major');
+    expect(diffWithUnstable('0.2.1-rc', '1.0.0-beta')).toBe('premajor');
+    expect(diffWithUnstable('0.2.2-rc.0', '0.2.2-rc.1')).toBe('prerelease');
+  });
+
+  it('treats patch and minor releases for pre-minor versions as major', () => {
+    expect(diffWithUnstable('0.0.1', '0.0.2')).toBe('major');
+    expect(diffWithUnstable('0.0.1', '0.0.2-rc')).toBe('premajor');
+    expect(diffWithUnstable('0.0.2', '0.1.0')).toBe('major');
+    expect(diffWithUnstable('0.0.2', '0.1.0-alpha')).toBe('premajor');
+    expect(diffWithUnstable('0.0.2', '1.0.0')).toBe('major');
+    expect(diffWithUnstable('0.0.2-rc', '1.0.0-beta')).toBe('premajor');
+    expect(diffWithUnstable('0.0.2-rc.1', '0.0.2-rc.2')).toBe('prerelease');
+  });
+
+  it('returns null for equal versions', () => {
+    expect(diffWithUnstable('1.0.0', '1.0.0')).toBeNull();
+    expect(diffWithUnstable('2.0.1-beta.0', '2.0.1-beta.0')).toBeNull();
+    expect(diffWithUnstable('0.2.1', '0.2.1')).toBeNull();
+    expect(diffWithUnstable('0.2.3-alpha', '0.2.3-alpha')).toBeNull();
+    expect(diffWithUnstable('0.0.11', '0.0.11')).toBeNull();
+    expect(diffWithUnstable('0.0.100-beta.2', '0.0.100-beta.2')).toBeNull();
   });
 });
