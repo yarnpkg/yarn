a23cc8e535afa5502e1cb9eb3269ed9fb392f7cd
Burak Yiğit Kaya <ben@byk.im>
2018-04-13 16:21:34 +0100

    fix(run): Remove notice and licenseText fields from npm_package_* (#5666)
    **Summary**
    
    Fixes #5420. NOTICE and licenseText for packages can be quite long which
    can cause E2BIG errors when trying to put it into environment variables
    when running scripts. Since these fields are not useful for scripts
    anyway, add them to the current blacklist which already has a similar
    field in it, `readme`.
    
    **Test plan**
    
    Added an automated test to ensure the env doesn't have these fields but
    keeps any other keys.

--- __tests__/util/execute-lifecycle-script.js
+++ __tests__/util/execute-lifecycle-script.js
@@ -1,6 +1,6 @@
 /* @flow */
 import path from 'path';
-import {makeEnv} from '../../src/util/execute-lifecycle-script';
+import {IGNORE_MANIFEST_KEYS, makeEnv} from '../../src/util/execute-lifecycle-script';
 import Config from '../../src/config';
 import {NoopReporter} from '../../src/reporters';
 
@@ -79,6 +79,24 @@ describe('makeEnv', () => {
       const config = await withManifest('', '', {'in^va!d_key': 'test'});
       const env = await makeEnv('', cwd, config);
       expect(env['npm_package_in_va_d_key']).toEqual('test');
+    });
+
+    it('it omits certain fields which tend to be too large', async () => {
+      const testManifest = {hello: 'I shall stay'};
+      for (const key of IGNORE_MANIFEST_KEYS) {
+        testManifest[key] = 'some long text we want to omit';
+        // ensure we don't carry an previously set environment variables for these
+        // to make tests consistent across runs with
+        delete process.env[`npm_package_${key}`];
+      }
+
+      const config = await withManifest('', '', testManifest);
+      const env = await makeEnv('', cwd, config);
+
+      expect(env).toHaveProperty('npm_package_hello', 'I shall stay');
+      for (const key of IGNORE_MANIFEST_KEYS) {
+        expect(env).not.toHaveProperty(`npm_package_${key}`);
+      }
     });
   });
 
