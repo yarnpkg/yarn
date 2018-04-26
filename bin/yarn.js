9c8f8177121c86caa9f9a0515e89daa71f8df7a1
Burak Yigit Kaya <ben@byk.im>
2018-04-26 10:19:19 +0100

    build(node4): Drop Node 4 builds which has expired
    Thanks for all the fish Node 4, you won't be missed.

--- bin/yarn.js
+++ bin/yarn.js
@@ -7,14 +7,16 @@
 var ver = process.versions.node;
 var majorVer = parseInt(ver.split('.')[0], 10);
 
-if (majorVer < 4) {
-  console.error('Node version ' + ver + ' is not supported, please use Node.js 4.0 or higher.');
+if (majorVer < 6) {
+  console.error('Node version ' + ver + ' is not supported, please use Node.js 6.0 or higher.');
   process.exit(1); // eslint-disable-line no-process-exit
 } else {
-  try {
-    require(__dirname + '/../lib/v8-compile-cache.js');
-  } catch (err) {
-    // We don't have/need this on legacy builds and dev builds
+  var dirPath = '../lib/';
+  var v8CompileCachePath = dirPath + 'v8-compile-cache';
+  var fs = require('fs');
+  // We don't have/need this on dev builds
+  if (fs.existsSync(v8CompileCachePath)) {
+    require(v8CompileCachePath);
   }
 
   // Just requiring this package will trigger a yarn run since the
