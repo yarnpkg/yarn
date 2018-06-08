1db709a0c3a19576d4e5bc6ac54d6c56debbc269
Maël Nison <nison.mael@gmail.com>
2018-06-08 16:48:44 +0100

    Update workspaces.js

--- __tests__/commands/workspaces.js
+++ __tests__/commands/workspaces.js
@@ -11,10 +11,10 @@ const fixturesLoc = path.join(__dirname, '..', 'fixtures', 'workspace');
 async function runWorkspaces(
   flags: Object,
   args: Array<string>,
-  cwdSubfolders: string[],
+  name: string,
   checkSteps?: ?(config: Config, reporter: BufferReporter) => ?Promise<void>,
 ): Promise<void> {
-  const cwd = path.join(fixturesLoc, ...cwdSubfolders);
+  const cwd = path.join(fixturesLoc, name);
   const reporter = new reporters.BufferReporter({stdout: null, stdin: null, isSilent: true});
 
   const config = await Config.create({cwd}, reporter);
@@ -27,7 +27,7 @@ async function runWorkspaces(
 }
 
 test('workspaces info should list the workspaces', (): Promise<void> => {
-  return runWorkspaces({}, ['info'], ['run-basic'], (config, reporter) => {
+  return runWorkspaces({}, ['info'], 'run-basic', (config, reporter) => {
     expect(reporter.getBufferJson()).toEqual({
       'workspace-1': {
         location: 'packages/workspace-child-1',
@@ -44,7 +44,7 @@ test('workspaces info should list the workspaces', (): Promise<void> => {
 });
 
 test('workspaces info should list the workspaces when launched from inside workspace folder', (): Promise<void> => {
-  return runWorkspaces({}, ['info'], ['run-variations', 'trailing-slash'], (config, reporter) => {
+  return runWorkspaces({}, ['info'], 'run-variations/trailing-slash', (config, reporter) => {
     expect(reporter.getBufferJson()).toEqual({
       'trailing-slash': {
         location: 'trailing-slash',
