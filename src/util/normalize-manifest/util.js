4adcc89ec2c8f270236cd0c541239868c4d675a2
mgquan@myseneca.ca <mgquan@myseneca.ca>
2018-07-21 15:15:22 -0400

    changed isValid check

--- src/util/normalize-manifest/util.js
+++ src/util/normalize-manifest/util.js
@@ -5,6 +5,14 @@ import type {PersonObject} from '../../types.js';
 const validateLicense = require('validate-npm-package-license');
 
 export function isValidLicense(license: string): boolean {
+  if (license.trim().length == 0) {
+    return true;
+  }
+
+  try {
+    validateLicense(license);
+  } catch (e) {}
+
   return !!license && validateLicense(license).validForNewPackages;
 }
 
