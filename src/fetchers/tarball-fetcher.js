08b73d1a1a99e022d150db0c78407009b7a814e3
Will Anderson <will@itsananderson.com>
2018-07-27 18:12:24 -0700

    Fix lints

--- src/fetchers/tarball-fetcher.js
+++ src/fetchers/tarball-fetcher.js
@@ -221,7 +221,6 @@ export default class TarballFetcher extends BaseFetcher {
   }
 
   _fetch(): Promise<FetchedOverride> {
-
     const isFilePath = this.reference.startsWith('file:');
     this.reference = removePrefix(this.reference, 'file:');
     const urlParse = url.parse(this.reference);
@@ -236,7 +235,7 @@ export default class TarballFetcher extends BaseFetcher {
     }
 
     return this.fetchFromLocal().catch(err => {
-      this.reporter.verbose(this.reporter.lang("errorFetchingFromLocal", err.message));
+      this.reporter.verbose(this.reporter.lang('errorFetchingFromLocal', err.message));
       this.reporter.verbose(err.message);
       return this.fetchFromExternal();
     });
