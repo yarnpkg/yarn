5512472fd3755133de41646d51884f229b753359
Vladimir Starkov <iamstarkov@gmail.com>
2018-08-06 23:38:39 +0200

    fix(cli-create): fix scoped creation
    This one works as it should:
    yarn create @scope/name => @scope/create-name
    
    There were problems when name was omitted:
    
    Before:
    yarn create @scope => create-
    yarn create @scope/ => @scope/create-
    
    After:
    yarn create @scope => @scope/create
    yarn create @scope/ => @scope/create
    
    Fixes #6233

--- src/cli/commands/create.js
+++ src/cli/commands/create.js
@@ -16,6 +16,42 @@ export function hasWrapper(commander: Object, args: Array<string>): boolean {
   return true;
 }
 
+const coerceName = (name = '') => (name === '' ? 'create' : `create-${name}`);
+const coerceScope = (scope = '') => (scope === '@' ? '' : scope);
+const coerceFullName = (scope, name) => [coerceScope(scope), coerceName(name)].filter(Boolean).join('/');
+
+/**
+ * # Tests
+ *
+ * ## basic
+ * parseBuilderName('name').packageName === 'create-name'
+ * parseBuilderName('@scope/name').packageName === '@scope/create-name'
+ *
+ * ## not adding "-" if name is empty
+ * parseBuilderName('@scope/').packageName === '@scope/create'
+ * parseBuilderName('@scope').packageName === '@scope/create'
+ *
+ * ## edge cases
+ * parseBuilderName('@/name').packageName === 'create-name'
+ * parseBuilderName('/name').packageName === 'create-name'
+ * parseBuilderName('@/').packageName === 'create'
+ */
+const parseBuilderName = str => {
+  const parts = str.split('/');
+  if (parts.length === 1 && !str.includes('@')) {
+    return {
+      packageName: coerceName(str),
+      packageDir: '',
+      commandName: coerceName(str),
+    };
+  }
+  return {
+    packageName: coerceFullName(parts[0], parts[1]),
+    packageDir: coerceScope(parts[0]),
+    commandName: coerceName(parts[1]),
+  };
+};
+
 export async function run(config: Config, reporter: Reporter, flags: Object, args: Array<string>): Promise<void> {
   const [builderName, ...rest] = args;
 
@@ -23,10 +59,7 @@ export async function run(config: Config, reporter: Reporter, flags: Object, arg
     throw new MessageError(reporter.lang('invalidPackageName'));
   }
 
-  const packageName = builderName.replace(/^(@[^\/]+\/)?/, '$1create-');
-  const packageDir = packageName.replace(/^(?:(@[^\/]+)\/)?.*/, '$1');
-  const commandName = packageName.replace(/^@[^\/]+\//, '');
-
+  const {packageName, packageDir, commandName} = parseBuilderName(builderName);
   await runGlobal(config, reporter, {}, ['add', packageName]);
 
   const binFolder = await getBinFolder(config, {});
