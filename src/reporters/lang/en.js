c67995f8f2ea6da309f83355644bc4a412fd4906
Maël Nison <nison.mael@gmail.com>
2018-10-01 16:39:55 +0100

    Update en.js

--- src/reporters/lang/en.js
+++ src/reporters/lang/en.js
@@ -358,7 +358,7 @@ const messages = {
   unplugDisabled: "Packages can only be unplugged when Plug'n'Play is enabled.",
 
   plugnplayWindowsSupport:
-    "Plug'n'Play is ignored on Windows for now - contributions welcome! https://github.com/yarnpkg/yarn/issues/6402",
+    "Plug'n'Play on Windows doesn't support the cache and project to be kept on separate drives",
 
   packageInstalledWithBinaries: 'Installed $0 with binaries:',
   packageHasBinaries: '$0 has binaries:',
