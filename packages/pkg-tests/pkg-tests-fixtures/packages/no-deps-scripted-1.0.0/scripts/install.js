e786a9bc31fe55aec31653df0bee1955ab45bfbe
Maël Nison <nison.mael@gmail.com>
2018-11-12 12:12:53 +0000

    Fixes write paths

--- packages/pkg-tests/pkg-tests-fixtures/packages/no-deps-scripted-1.0.0/scripts/install.js
+++ packages/pkg-tests/pkg-tests-fixtures/packages/no-deps-scripted-1.0.0/scripts/install.js
@@ -1,3 +1,3 @@
 const fs = require(`fs`);
 
-fs.appendFileSync(`${__dirname}/log.js`, `module.exports.push('install');`);
+fs.appendFileSync(`${__dirname}/../log.js`, `module.exports.push('install');`);
