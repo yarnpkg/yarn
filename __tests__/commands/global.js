01e5f4872e030720d12d4f8d3c51698735a1856f
Brian Ricketts <bricketts@millinfo.com>
2019-01-24 11:43:13 -0600

    Fix PowerShell test on non-Windows platforms

--- __tests__/commands/global.js
+++ __tests__/commands/global.js
@@ -94,9 +94,10 @@ test.concurrent("shouldn't create powershell shims", async (): Promise<void> => 
   const tmpGlobalFolder = await createTempGlobalFolder();
   const tmpPrefixFolder = await createTempPrefixFolder();
   const flags = {globalFolder: tmpGlobalFolder, prefix: tmpPrefixFolder};
+  const isWindows = process.platform === "win32";
   return runGlobal(['add', 'react-native-cli'], flags, 'add-with-prefix-flag', async config => {
     expect(await fs.exists(path.join(tmpPrefixFolder, 'bin', 'react-native'))).toEqual(true);
-    expect(await fs.exists(path.join(tmpPrefixFolder, 'bin', 'react-native.cmd'))).toEqual(true);
+    expect(await fs.exists(path.join(tmpPrefixFolder, 'bin', 'react-native.cmd'))).toEqual(isWindows);
 
     // Should not have any ps1 shims whatsoever (including erroneous double-extension ones)
     expect(await fs.exists(path.join(tmpPrefixFolder, 'bin', 'react-native.ps1'))).toEqual(false);
