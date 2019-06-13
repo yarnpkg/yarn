1907231feac1282ab1d938ff39e6bac925a6cba1
Maël Nison <nison.mael@gmail.com>
2019-06-13 16:58:49 +0200

    Stops reading the manifest for link: dependencies

--- src/resolvers/exotics/link-resolver.js
+++ src/resolvers/exotics/link-resolver.js
@@ -30,9 +30,7 @@ export default class LinkResolver extends ExoticResolver {
     const name = path.basename(loc);
     const registry: RegistryNames = 'npm';
 
-    const manifest: Manifest = !await fs.exists(loc)
-      ? {_uid: '', name, version: '0.0.0', _registry: registry}
-      : await this.config.readManifest(loc, this.registry);
+    const manifest: Manifest = {_uid: '', name: 'pkg', version: '0.0.0', _registry: registry};
 
     manifest._remote = {
       type: 'link',
