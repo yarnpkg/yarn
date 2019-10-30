yarnpkg/yarn

commit aebe9e9601a077d13401bf161f756f0a767a2adc

fix(modules-folder): Fix --modules-folder handling in several places. (#6850)
* fix(modules-folder): Fix --modules-folder handling in several places.

Fixes a few issues with the `--modules-folder` parameter.

The extraneous file check would always
also clean files from `node_modules`. This no longer happens, only the `--modules-folder` location
is checked.

The `yarn check` command used to always only check `node_modules`. Now it will check the
`--modules-folder` location instead.

When running a command or executing a lifecycle script, yarn
would build a `PATH` that included `node_modules/.bin` first, then add the `--modules-folder`'s
`/.bin` after, which would have led to the wrong binaries being executed.

fixes #5419 and fixes #6847

* update changelog

* fix --moduls-folder handling in bin and run commands

src/cli/commands/run.js
@@ -5,7 +5,6 @@ import type Config from '../../config.js';
 import {execCommand, makeEnv} from '../../util/execute-lifecycle-script.js';
 import {dynamicRequire} from '../../util/dynamic-require.js';
 import {MessageError} from '../../errors.js';
-import {registries} from '../../resolvers/index.js';
 import * as fs from '../../util/fs.js';
 import * as constants from '../../constants.js';
 
@@ -29,8 +28,8 @@ export async function getBinEntries(config: Config): Promise<Map<string, string>
   const binEntries = new Map();
 
   // Setup the node_modules/.bin folders for analysis
-  for (const registry of Object.keys(registries)) {
-    binFolders.add(path.join(config.cwd, config.registries[registry].folder, '.bin'));
+  for (const registryFolder of config.registryFolders) {
+    binFolders.add(path.resolve(config.lockfileFolder, registryFolder, '.bin'));
   }
 
   // Same thing, but for the pnp dependencies, located inside the cache

__tests__/commands/_helpers.js
@@ -97,6 +97,7 @@ export function makeConfigFromDirectory(cwd: string, reporter: Reporter, flags: 
       focus: !!flags.focus,
       enableDefaultRc: !flags.noDefaultRc,
       extraneousYarnrcFiles: flags.useYarnrc,
+      modulesFolder: flags.modulesFolder ? path.join(cwd, flags.modulesFolder) : undefined,
     },
     reporter,
   );

__tests__/commands/install/integration.js
@@ -1140,6 +1140,19 @@ test('install will not warn for missing optional peer dependencies', () =>
     expect(warningMessage).toBe(false);
   }));
 
+test('does not check node_modules for extraneous files when --modules-folder used', async () => {
+  // Scenario: https://github.com/yarnpkg/yarn/issues/5419
+  // When `--modules-foler` is passed, yarn should check that directory for extraneous files.
+  // Also, the default node_modules dir, if it exists, should not be cleaned out (marked as extraneous).
+  await runInstall({modulesFolder: './some_modules'}, 'extraneous-node-modules', async (config): Promise<void> => {
+    expect(await fs.exists(`${config.cwd}/some_modules/feed`)).toEqual(true);
+    // Extraneous files in node_modules should not have been cleaned.
+    expect(await fs.exists(`${config.cwd}/node_modules/extra.js`)).toEqual(true);
+    // Extraneous files in some_modules should have been cleaned.
+    expect(await fs.exists(`${config.cwd}/some_modules/extra.js`)).toEqual(false);
+  });
+});
+
 test('install skips the scripts if the yarnrc specifies skip-scripts true', () =>
   runInstall({}, 'ignore-scripts-by-yarnrc', (config, reporter, install, getStdout) => {
     const stdout = getStdout();

src/cli/commands/autoclean.js
@@ -78,12 +78,8 @@ export async function clean(
 
   // build list of possible module folders
   const locs = new Set();
-  if (config.modulesFolder) {
-    locs.add(config.modulesFolder);
-  }
-  for (const name of registryNames) {
-    const registry = config.registries[name];
-    locs.add(path.join(config.lockfileFolder, registry.folder));
+  for (const registryFolder of config.registryFolders) {
+    locs.add(path.resolve(config.lockfileFolder, registryFolder));
   }
 
   const workspaceRootFolder = config.workspaceRootFolder;

src/cli/commands/bin.js
@@ -2,7 +2,6 @@
 
 import type {Reporter} from '../../reporters/index.js';
 import type Config from '../../config.js';
-import RegistryYarn from '../../resolvers/registries/yarn-resolver.js';
 import {getBinEntries} from './run.js';
 
 const path = require('path');
@@ -16,7 +15,7 @@ export function setFlags(commander: Object) {
 }
 
 export async function run(config: Config, reporter: Reporter, flags: Object, args: Array<string>): Promise<void> {
-  const binFolder = path.join(config.cwd, config.registries[RegistryYarn.registry].folder, '.bin');
+  const binFolder = path.join(config.cwd, config.registryFolders[0], '.bin');
   if (args.length === 0) {
     reporter.log(binFolder, {force: true});
   } else {

src/cli/commands/check.js
@@ -39,7 +39,7 @@ export async function verifyTreeCheck(
   }
   // check all dependencies recursively without relying on internal resolver
   const registryName = 'yarn';
-  const registry = config.registries[registryName];
+  const registryFolder = config.registryFolders[0];
   const cwd = config.workspaceRootFolder ? config.lockfileFolder : config.cwd;
   const rootManifest = await config.readManifest(cwd, registryName);
 
@@ -86,7 +86,7 @@ export async function verifyTreeCheck(
   const locationsVisited: Set<string> = new Set();
   while (dependenciesToCheckVersion.length) {
     const dep = dependenciesToCheckVersion.shift();
-    const manifestLoc = path.join(dep.parentCwd, registry.folder, dep.name);
+    const manifestLoc = path.resolve(dep.parentCwd, registryFolder, dep.name);
     if (locationsVisited.has(manifestLoc + `@${dep.version}`)) {
       continue;
     }
@@ -114,19 +114,19 @@ export async function verifyTreeCheck(
     const dependencies = pkg.dependencies;
     if (dependencies) {
       for (const subdep in dependencies) {
-        const subDepPath = path.join(manifestLoc, registry.folder, subdep);
+        const subDepPath = path.resolve(manifestLoc, registryFolder, subdep);
         let found = false;
         const relative = path.relative(cwd, subDepPath);
-        const locations = path.normalize(relative).split(registry.folder + path.sep).filter(dir => !!dir);
+        const locations = path.normalize(relative).split(registryFolder + path.sep).filter(dir => !!dir);
         locations.pop();
         while (locations.length >= 0) {
           let possiblePath;
           if (locations.length > 0) {
-            possiblePath = path.join(cwd, registry.folder, locations.join(path.sep + registry.folder + path.sep));
+            possiblePath = path.join(cwd, registryFolder, locations.join(path.sep + registryFolder + path.sep));
           } else {
             possiblePath = cwd;
           }
-          if (await fs.exists(path.join(possiblePath, registry.folder, subdep))) {
+          if (await fs.exists(path.resolve(possiblePath, registryFolder, subdep))) {
             dependenciesToCheckVersion.push({
               name: subdep,
               originalKey: `${dep.originalKey}#${subdep}`,

src/config.js
@@ -317,7 +317,7 @@ export default class Config {
     }
 
     if (this.modulesFolder) {
-      this.registryFolders.push(this.modulesFolder);
+      this.registryFolders = [this.modulesFolder];
     }
 
     this.networkConcurrency =

src/util/execute-lifecycle-script.js
@@ -7,7 +7,6 @@ import * as child from './child.js';
 import * as fs from './fs.js';
 import {dynamicRequire} from './dynamic-require.js';
 import {makePortableProxyScript} from './portable-script.js';
-import {registries} from '../resolvers/index.js';
 import {fixCmdWinSlashes} from './fix-cmd-win-slashes.js';
 import {getBinFolder as getGlobalBinFolder, run as globalRun} from '../cli/commands/global.js';
 
@@ -191,16 +190,13 @@ export async function makeEnv(
   }
 
   // Add node_modules .bin folders to the PATH
-  for (const registry of Object.keys(registries)) {
-    const binFolder = path.join(config.registries[registry].folder, '.bin');
+  for (const registryFolder of config.registryFolders) {
+    const binFolder = path.join(registryFolder, '.bin');
     if (config.workspacesEnabled && config.workspaceRootFolder) {
       pathParts.unshift(path.join(config.workspaceRootFolder, binFolder));
     }
     pathParts.unshift(path.join(config.linkFolder, binFolder));
     pathParts.unshift(path.join(cwd, binFolder));
-    if (config.modulesFolder) {
-      pathParts.unshift(path.join(config.modulesFolder, '.bin'));
-    }
   }
 
   let pnpFile;

commit 79a96e31321e58e650e5e879bef52d57dda746af

fix(run): add cwd/node_modules/.bin to run command search path (#7151)
* fix(run): change run command to check cwd/node_modules/.bin for commands. Fixes run in workspaces.

This fixes a bug that was introduced in #6850 where the bin path was being built only from
`config.lockfileFolder`. However in workspaces, bins may not be hoisted to the workspace root,
causing bins to not be found. This change adds `config.cwd` to the bin search path, so the `yarn
run` command will look in a workspace package's node_modules, as well as the workspace root.

fixes #7126

* modify chagelog

src/cli/commands/run.js
@@ -31,6 +31,7 @@ export async function getBinEntries(config: Config): Promise<Map<string, string>
 
   // Setup the node_modules/.bin folders for analysis
   for (const registryFolder of config.registryFolders) {
+    binFolders.add(path.resolve(config.cwd, registryFolder, '.bin'));
     binFolders.add(path.resolve(config.lockfileFolder, registryFolder, '.bin'));
   }
 

__tests__/commands/run.js
@@ -207,6 +207,16 @@ test('adds workspace root node_modules/.bin to path when in a workspace', (): Pr
     expect(envPaths).toContain(path.join(config.cwd, 'packages', 'pkg1', 'node_modules', '.bin'));
   }));
 
+test('adds cwd node_modules/.bin to path when in a workspace usig nohoist', (): Promise<void> =>
+  runRunInWorkspacePackage('packages/pkg1', ['env'], {}, 'nohoist-workspace', (config, reporter): ?Promise<void> => {
+    const logEntry = reporter.getBuffer().find(entry => entry.type === 'log');
+    const parsedLogData = JSON.parse(logEntry ? logEntry.data.toString() : '{}');
+    const envPaths = (parsedLogData.PATH || parsedLogData.Path).split(path.delimiter);
+
+    expect(envPaths).toContain(path.join(config.cwd, 'node_modules', '.bin'));
+    expect(envPaths).toContain(path.join(config.cwd, 'packages', 'pkg1', 'node_modules', '.bin'));
+  }));
+
 test('runs script with custom script-shell', (): Promise<void> =>
   runRunWithCustomShell('/usr/bin/dummy', ['start'], {}, 'script-shell', async (config): ?Promise<void> => {
     const pkg = await fs.readJson(path.join(config.cwd, 'package.json'));

__tests__/fixtures/request-cache/GET/gitlab.com/leanlabsio/kanban/raw/f139bd887dea8e48c46fd7fcfe42b5ffc53d79dd/package.json.bin
@@ -1,0 +1,56 @@
+HTTP/1.1 200 OK
+Server: nginx
+Date: Wed, 27 Mar 2019 12:51:12 GMT
+Content-Type: text/plain; charset=utf-8
+Content-Length: 1181
+Cache-Control: max-age=3600, public
+Content-Disposition: inline
+Etag: W/"78309fbf8af4479c47eca65b0c5e3f51"
+X-Content-Type-Options: nosniff
+X-Frame-Options: DENY
+X-Request-Id: kTsEGIR9zSa
+X-Runtime: 0.099649
+X-Ua-Compatible: IE=edge
+X-Xss-Protection: 1; mode=block
+Strict-Transport-Security: max-age=31536000
+Content-Security-Policy: object-src 'none'; worker-src https://assets.gitlab-static.net https://gl-canary.freetls.fastly.net https://gitlab.com blob:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://assets.gitlab-static.net https://gl-canary.freetls.fastly.net https://www.google.com/recaptcha/ https://www.recaptcha.net/ https://www.gstatic.com/recaptcha/ https://apis.google.com; style-src 'self' 'unsafe-inline' https://assets.gitlab-static.net https://gl-canary.freetls.fastly.net; img-src * data: blob:; frame-src 'self' https://www.google.com/recaptcha/ https://www.recaptcha.net/ https://content.googleapis.com https://content-compute.googleapis.com https://content-cloudbilling.googleapis.com https://content-cloudresourcemanager.googleapis.com https://*.codesandbox.io; frame-ancestors 'self'; connect-src 'self' https://assets.gitlab-static.net https://gl-canary.freetls.fastly.net wss://gitlab.com https://sentry.gitlab.net https://customers.gitlab.com https://snowplow.trx.gitlab.net
+
+{
+  "name": "kanban",
+  "version": "0.0.1",
+  "repository": "gitlab.com/leanlabsio/kanban",
+  "scripts": {
+      "install": "npm install",
+      "build": "grunt build",
+      "watch": "grunt watch"
+  },
+  "devDependencies": {
+    "grunt": "~0.4.1",
+    "grunt-cli": "~0.1.13",
+    "grunt-contrib-copy": "^0.5.0",
+    "grunt-contrib-concat": "~0.5.0",
+    "grunt-contrib-watch": "~0.5.3",
+    "grunt-contrib-uglify": "~0.7.0",
+    "grunt-sass": "1.0.0",
+    "grunt-contrib-connect": "~0.8.0",
+    "grunt-connect-proxy": "~0.1.11"
+  },
+  "dependencies": {
+    "angular": "=1.5.6",
+    "angular-lodash": "https://github.com/EMSSConsulting/angular-lodash.git#68a726c",
+    "foundation-sites": "5.5.2",
+    "angular-foundation": "https://github.com/pineconellc/angular-foundation.git#8f3f260",
+    "angular-loading-bar": "=0.5.2",
+    "angular-storage": "=0.0.6",
+    "angular-ui-router": "=0.3.0",
+    "angularjs-datepicker": "=0.2.15",
+    "font-awesome": "=4.6.3",
+    "markdown-it": "=5.0.2",
+    "markdown-it-emoji": "=1.1.0",
+    "ng-sortable": "=1.3.6",
+    "sass-flex-mixin": "=1.0.3",
+    "lodash": "=4.13.1",
+    "twemoji": "=2.1.0",
+    "angular-file-upload": "=2.3.4"
+  }
+}

__tests__/fixtures/request-cache/GET/registry.yarnpkg.com/is-pnp.bin
@@ -1,0 +1,18 @@
+HTTP/1.1 200 OK
+Date: Wed, 27 Mar 2019 12:49:49 GMT
+Content-Type: application/vnd.npm.install-v1+json
+Content-Length: 4072
+Connection: keep-alive
+Set-Cookie: __cfduid=d2e7739722b1e2621b032ee7a09984edd1553690988; expires=Thu, 26-Mar-20 12:49:48 GMT; path=/; domain=.registry.yarnpkg.com; HttpOnly
+CF-Cache-Status: REVALIDATED
+Cache-Control: max-age=300
+CF-Ray: 4be17c86d97f56c3-IAD
+Accept-Ranges: bytes
+ETag: "49fffc656197ace1f624570132931c23"
+Expect-CT: max-age=604800, report-uri="https://report-uri.cloudflare.com/cdn-cgi/beacon/expect-ct"
+Last-Modified: Thu, 01 Nov 2018 01:11:09 GMT
+Vary: accept-encoding, accept
+x-amz-meta-rev: 3-72d30e72712ca5e8a55f6613d7a30236
+Server: cloudflare
+
+{"versions":{"1.0.0":{"name":"is-pnp","version":"1.0.0","bin":{"is-pnp":"./bin.js"},"_hasShrinkwrap":false,"directories":{},"dist":{"shasum":"47d3d7151df242eb7ca4768fc2e7f6228871899a","integrity":"sha512-Lx0Sh5h20HtLz+xnlh4NRhQ6w231tGN4yuU3oBi6vn7oSbiIk2V4sqoIn00bczm9ojwkFPzzFUeSOIKEgYpszA==","tarball":"https://registry.npmjs.org/is-pnp/-/is-pnp-1.0.0.tgz","fileCount":5,"unpackedSize":2268,"npm-signature":"-----BEGIN PGP SIGNATURE-----\r\nVersion: OpenPGP.js v3.0.4\r\nComment: https://openpgpjs.org\r\n\r\nwsFcBAEBCAAQBQJb2lGmCRA9TVsSAnZWagAARHYP/1botUA//2RoqJEP3nHB\nnQ0G1qhSYL685aLgZTWJW9pS3ucPzxxeofbnByOsbSjUqycwsHBqL/DDeYsq\nz3b0dmfcfESK+CSafYECSnj8kPJH5N5VU2UXs+A+wLEM0D0nSe4wfJLVptA0\nrOpeYrkfwX8ZC1EGLGvQM4cMqpk3Ji6RBZHtD0U/ttYR3A0vP5P76oO2/tBl\n3QSK3nJSpU83OkMBPVY3hnDrGkCjJqwvcqWoWUG0/BtHS5KYD99yrxFAzT5f\nMhtqCZiediDcGtrMhP5b0VAkqdrwFPg/tZPpgqYCevR+2ipWhkxhD0Z4yyx1\n2AuJnEoQKRiBiFPIv5aiJZD9Jj00iey4tdQ1NcXnRtAA16Lo0t4MX/OnkyJW\nBfcSd795mVpe4AO9u6bqMDZfkQrBRunxa7OKKCEXUGihm0YNtTWKat5ym1ia\n9H9Bwlgm3GoFAuHQMu07ZEwidLpWoQfjrTUOufw+/rKNSIRoMNuL3Z/8i4Hj\nazYRMgdzb9rkf6+F7LLJKnyZaDY/bS/KIb61LdkfkX8JCcKUfhVAd5KIvI4e\nAwLAUfHwWM2u2smbexFzvgvXnNFmVrcCGBbMcIrQ+JfiJ+izFVJ7YyLY+Fpt\nwxbV0Vug6gyYDyDtjFCdeL2wFmgR3+nASXWFY4CZ+ZoFX+ontSZ4U5b9PTIs\n1ojq\r\n=DAoM\r\n-----END PGP SIGNATURE-----\r\n"},"engines":{"node":">=6"}},"1.0.1":{"name":"is-pnp","version":"1.0.1","bin":{"is-pnp":"./sources/cli.js"},"_hasShrinkwrap":false,"directories":{},"dist":{"shasum":"ec0e4e443287214ce90bcb6fcd112d20de51f120","integrity":"sha512-uwFQ9SabYoNJwZnDqHUs+WTPvegmoLHjVQqkX4+MiKSjhRBRk+n/zKBAex4yvWnzbN352W7FUuYX8L5yEVmiVA==","tarball":"https://registry.npmjs.org/is-pnp/-/is-pnp-1.0.1.tgz","fileCount":5,"unpackedSize":2276,"npm-signature":"-----BEGIN PGP SIGNATURE-----\r\nVersion: OpenPGP.js v3.0.4\r\nComment: https://openpgpjs.org\r\n\r\nwsFcBAEBCAAQBQJb2lJICRA9TVsSAnZWagAAX2IP/jE0+3aAdO7cu9DXNJnb\njoi3rDo8CRfARhWbJRhUj7omDxeP37U3dIIkK69AL8XZSPPUQ8o2uH1AOjZB\niI8cp0IX4bgalRXzEWPj3v8l3z6/BwJoUXy7N5ugdxron+AYj4kS4I1jI6Pa\nUo3z096sc8zAgXFE00RiKwUBGNw+4xpg0yVri/vXpezsahi8r4qZbrc9/hr3\nWzoIK6GwQnGpn5FEhdiuPbB0QiZvalYHn5v2IWtMpQG4ed6jM/vSBAqKqkFv\n95McUD13wz7iJ59BaJHwbypD/WFWz34ZBPZ8QpONk6UAJ2IZwh6J2Wj44k7a\nNtiEj3xo3A5stikfELI//H0AruTI6sSI++EJ8YvHQUj73s6kG0Z/FzPptgKp\nKYLSqXNAC/zHiId1s7uj+jsCLXIm4G2bPQUTsDMfejDS6TfjAVO2COxRF34q\ncLGzYFL1/R+/hO4ckoLl7vz6Vm/zSlMaxZPDUFDy8d0ScxHZOL+760cdIhjT\nr1XGyXAmyjGLWTHvA6sE5X0H0PgghQErsLYXi679KBdnZjN2+Xcmv0pNIqbg\nwS7SBjV+KGpc1PnwtZdfvZcegvY+252zsBvRye9/hcQ0Rdmetbod8XicA3Bw\nuTLC2N9opHeTI7a9bKrACXkSApJftjzYSfnycb27r9bgIAY6BeNhr7JFEMKV\nnxUD\r\n=8Blc\r\n-----END PGP SIGNATURE-----\r\n"},"engines":{"node":">=6"}},"1.0.2":{"name":"is-pnp","version":"1.0.2","bin":{"is-pnp":"./sources/cli.js"},"_hasShrinkwrap":false,"directories":{},"dist":{"shasum":"cbe5d6ad751897822fd92539ac5cfa37c04f3852","integrity":"sha512-BzoewUq0EZFJYKlYpObv2xRdTRJQXwMLbk2Y5l8Dhl0EQGPhdPrSdBlQaAT46gIKjAkNBNScDiUDbuBCTDNCsQ==","tarball":"https://registry.npmjs.org/is-pnp/-/is-pnp-1.0.2.tgz","fileCount":5,"unpackedSize":2282,"npm-signature":"-----BEGIN PGP SIGNATURE-----\r\nVersion: OpenPGP.js v3.0.4\r\nComment: https://openpgpjs.org\r\n\r\nwsFcBAEBCAAQBQJb2lKpCRA9TVsSAnZWagAAkFcP+QHfioEIu01ww57y/Jnw\neOtM4JPvx0hDMKxMBKSyvVHmjZKwqxvkAnqdyybHts7i6auvuuMKDO43oaRO\n5GY5i/u29arwBKnPgeVrC6TUnbVxiSP4KBtfzRs3YCXbQ0MfSPmXiH4nzzi/\ngWeNGutgCAbefJjB0eahVCUgO7DL+sM8kEt2iNSDM1gKUDSVq5yvsZD20tX+\n/60eLG4xtz/2zXDKjoqo+DZtvW9kdLJ6TlvbDU1mt1OBSzkTiakIe1XXymVT\n+gRC0QGKGMP+YJG+VothHez/JAagl/92c/JDsb54HkW/OkUKCHFyUAw0P3h1\n46p0vEr54rHa7wT6O0fKg7q+MN1mMOKR3wO258LQW20n+DQJK1hxcJOkk+xf\n6iOa2d6Ywox9yT9B/V555BEZ2fYbnNb4gw+oo3/YyJcZ1Ooadt5kvEyDb67+\ntOaOMBXZh7/FR8EghwBBqlOndWCD4OQYVXVCwIdf/PFyUtB1+Obd4/1H+Dkw\nmg01pi7jYHG8mKLIMNdjggWi966nJ3fZqBDLW0eRtFzCbe+8VSOPKGRSHHnI\n99SD9oY/dgmimcakZ3hInuGlMGf0UrSLjHwi24y0ae1O0AlnQz0bXU0qHAH2\nH/Npr3zjOcAULwvHqtu2l1mNv/PX8GtafyybiMaXBcERearcUN/48tbvcbFX\nZmBP\r\n=z5fE\r\n-----END PGP SIGNATURE-----\r\n"},"engines":{"node":">=6"}}},"name":"is-pnp","dist-tags":{"latest":"1.0.2"},"modified":"2018-11-01T01:11:07.975Z"}