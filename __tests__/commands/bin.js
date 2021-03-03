26dc660ad4850a03c066a5da8a7befb3352bae82
Seung-gi Lim <seung.gi.lim@gmail.com>
2021-03-03 18:07:55 +0900

    Use cwd node_modules/.bin entry first in workspace
    - If there are multiple project which has dependency for the different
      version of 3rd party modules, only workspace root .bin was resolved.
    - This change fixes this bug by changing order of bin entries when
      resolving binary locations which are in several different package
      folders
    - See https://github.com/sglim/yarn-workspace-bin-bug to reproduce the
      bug

--- __tests__/commands/bin.js
+++ __tests__/commands/bin.js
@@ -10,6 +10,15 @@ const fixturesLoc = path.join(__dirname, '..', 'fixtures', 'bin');
 const runBin = buildRun.bind(null, BufferReporter, fixturesLoc, (args, flags, config, reporter): Promise<void> => {
   return run(config, reporter, flags, args);
 });
+const runInWorkspacePackage = function(cwd, config, reporter, flags, args): Promise<void> {
+  const originalCwd = config.cwd;
+  config.cwd = path.join(originalCwd, cwd);
+  const retVal = run(config, reporter, flags, args);
+  retVal.then(() => {
+    config.cwd = originalCwd;
+  });
+  return retVal;
+};
 
 test('running bin without arguments should return the folder where the binaries are stored', (): Promise<void> => {
   return runBin([], {}, '../install/install-production-bin', (config, reporter): ?Promise<void> => {
@@ -22,5 +31,17 @@ test('running bin with a binary name as the argument should return its full path
     const reporter = new BufferReporter();
     await run(config, reporter, {}, ['rimraf']);
     expect(reporter.getBufferText()).toMatch(/[\\\/]node_modules[\\\/]\.bin[\\\/]rimraf$/);
+  });
+});
+
+test('should use .bin in workspace node modules respectively', (): Promise<void> => {
+  return runInstall({binLinks: true}, 'workspaces-install-link-bin-2-versions', async (config): ?Promise<void> => {
+    const reporter1 = new BufferReporter();
+    await runInWorkspacePackage('packages/workspace-1', config, reporter1, {}, ['myBin']);
+    expect(reporter1.getBufferText()).toMatch(/[\\\/]workspace-1[\\\/]node_modules[\\\/]\.bin[\\\/]myBin$/);
+
+    const reporter2 = new BufferReporter();
+    await runInWorkspacePackage('packages/workspace-2', config, reporter2, {}, ['myBin']);
+    expect(reporter2.getBufferText()).toMatch(/[\\\/]workspace-2[\\\/]node_modules[\\\/]\.bin[\\\/]myBin$/);
   });
 });
