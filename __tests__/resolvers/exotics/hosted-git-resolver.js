7e7902fe6a2d111bcf5e8d7540bfd4211b268061
mmmsssttt404 <931121963@qq.com>
2025-08-13 10:04:26 +0800

    Update hosted-git-resolver.js

--- __tests__/resolvers/exotics/hosted-git-resolver.js
+++ __tests__/resolvers/exotics/hosted-git-resolver.js
@@ -28,13 +28,3 @@ const reporter = new reporters.NoopReporter({});
     expect(explodeHostedGitFragment(fragment, reporter).hash).toEqual(hash);
   });
 });
-describe('explodeHostedGitFragment DOS vulnerability test', () => {
-  const MAX_MS = 200;
-  test('long fragment without # should finish quickly and throw', () => {
-    const longFragment = '' + '\u0000'.repeat(100000) + '\u0000';
-    const start = Date.now();
-    expect(() => explodeHostedGitFragment(longFragment, reporter)).toThrow();
-    const duration = Date.now() - start;
-    expect(duration).toBeLessThan(MAX_MS);
-  });
-});
